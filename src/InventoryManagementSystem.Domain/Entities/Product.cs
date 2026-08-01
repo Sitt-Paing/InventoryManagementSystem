171d3bb8bc4364fdbda8e37d12f2d89bb694c84e
Sitt Paing <sittp9288@gmail.com>
2026-08-01 13:28:29 +0630

    Create Product.cs

---
+++ src/InventoryManagementSystem.Domain/Entities/Product.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using InventoryManagementSystem.Domain.Common;
+
+namespace InventoryManagementSystem.Domain.Entities;
+
+public partial class Product : BaseAuditableEntity<string>
+{
+    public string Name { get; set; } = null!;
+
+    public string? Sku { get; set; }
+
+    public long CategoryId { get; set; }
+
+    public decimal UnitPrice { get; set; }
+
+    public int CurrentStock { get; set; }
+
+    public int ReorderLevel { get; set; }
+
+    [JsonIgnore]
+    public virtual Category Category { get; set; } = null!;
+
+    [JsonIgnore]
+    public virtual ICollection<StockTransaction> StockTransactions { get; set; } = new List<StockTransaction>();
+}
