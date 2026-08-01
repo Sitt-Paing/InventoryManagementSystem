a95e7aa52eb178ae73cd75690287df4e05d49aa9
Sitt Paing <sittp9288@gmail.com>
2026-08-01 13:27:58 +0630

    Create AspNetRole.cs

---
+++ src/InventoryManagementSystem.Domain/Entities/AspNetRole.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace InventoryManagementSystem.Domain.Entities;
+
+public partial class AspNetRole
+{
+    public string Id { get; set; } = null!;
+
+    public string? Name { get; set; }
+
+    public string? NormalizedName { get; set; }
+
+    public string? ConcurrencyStamp { get; set; }
+
+    [JsonIgnore]
+    public virtual ICollection<AspNetRoleClaim> AspNetRoleClaims { get; set; } = new List<AspNetRoleClaim>();
+
+    [JsonIgnore]
+    public virtual ICollection<AspNetUser> Users { get; set; } = new List<AspNetUser>();
+}
