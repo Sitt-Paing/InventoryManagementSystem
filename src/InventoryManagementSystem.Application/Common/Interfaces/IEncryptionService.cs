f679265573f6c4571e7887d35762d269c9fa4a07
Sitt Paing <sittp9288@gmail.com>
2026-08-01 13:27:38 +0630

    Create IEncryptionService.cs

---
+++ src/InventoryManagementSystem.Application/Common/Interfaces/IEncryptionService.cs
@@ -0,0 +1,7 @@
+namespace InventoryManagementSystem.Application.Common.Interfaces;
+
+public interface IEncryptionService
+{
+    string Encrypt(string plainText);
+    string Decrypt(string cipherText);
+}
