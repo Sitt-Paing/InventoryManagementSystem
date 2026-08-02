59ebf403b83f20d1bf13d35e4b77581b59ebb3c3
Sitt Paing <sittp9288@gmail.com>
2026-08-02 17:28:01 +0630

    Update CategoriesController.cs

--- src/InventoryManagementSystem.Api/Controllers/CategoriesController.cs
+++ src/InventoryManagementSystem.Api/Controllers/CategoriesController.cs
@@ -7,15 +7,15 @@ namespace InventoryManagementSystem.Api.Controllers;
 
 public class CategoriesController : ApiControllerBase
 {
-    // GET: api/categories
     [HttpGet]
+    [EndpointSummary("Get all categories")]
     public async Task<ActionResult<List<CategoryDto>>> GetCategories()
     {
         return await Mediator.Send(new GetCategoriesQuery());
     }
 
-    // GET: api/categories/5
-    [HttpGet("{id:long}")]
+    [HttpGet("{id}")]
+    [EndpointSummary("Get category by Id")]
     public async Task<ActionResult<CategoryDto>> GetCategoryById(long id)
     {
         var result = await Mediator.Send(new GetCategoryByIdQuery(id));
@@ -24,16 +24,16 @@ public class CategoriesController : ApiControllerBase
         return Ok(result);
     }
 
-    // POST: api/categories
     [HttpPost]
+    [EndpointSummary("Create new category")]
     public async Task<ActionResult<long>> CreateCategory([FromBody] CreateCategoryCommand command)
     {
         var categoryId = await Mediator.Send(command);
         return CreatedAtAction(nameof(GetCategoryById), new { id = categoryId }, categoryId);
     }
 
-    // PUT: api/categories/5
-    [HttpPut("{id:long}")]
+    [HttpPut("{id}")]
+    [EndpointSummary("Update category")]
     public async Task<IActionResult> UpdateCategory(long id, [FromBody] UpdateCategoryCommand command)
     {
         if (id != command.Id) return BadRequest("Mismatched Category Id in route and body.");
@@ -44,8 +44,8 @@ public class CategoriesController : ApiControllerBase
         return NoContent();
     }
 
-    // DELETE: api/categories/5
     [HttpDelete("{id:long}")]
+    [EndpointSummary("Delete Category")]
     public async Task<IActionResult> DeleteCategory(long id)
     {
         var success = await Mediator.Send(new DeleteCategoryCommand(id));
