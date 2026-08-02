a0172f8f050a0e8f00a3b97666fe3aa9987e0ec9
Sitt Paing <sittp9288@gmail.com>
2026-08-02 13:02:41 +0630

    Create DependencyInjection.cs

---
+++ src/InventoryManagementSystem.Application/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace InventoryManagementSystem.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(cfg => {
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                cfg.AddBehavior(typeof(MediatR.IPipelineBehavior<,>), typeof(Common.Behaviors.ValidationBehavior<,>));
+            });
+            return services;
+        }
+    }
+}
