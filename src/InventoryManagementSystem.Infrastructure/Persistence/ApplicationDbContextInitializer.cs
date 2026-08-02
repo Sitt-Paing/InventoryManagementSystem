86d6dd89c9c16656778575ae8f451e5c5c829c37
Sitt Paing <sittp9288@gmail.com>
2026-08-02 13:03:29 +0630

    Create ApplicationDbContextInitializer.cs

---
+++ src/InventoryManagementSystem.Infrastructure/Persistence/ApplicationDbContextInitializer.cs
@@ -0,0 +1,76 @@
+using InventoryManagementSystem.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace InventoryManagementSystem.Infrastructure.Persistence;
+
+public class ApplicationDbContextInitializer
+{
+    private readonly ILogger<ApplicationDbContextInitializer> _logger;
+    private readonly ApplicationDbContext _context;
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public ApplicationDbContextInitializer(
+        ILogger<ApplicationDbContextInitializer> logger,
+        ApplicationDbContext context,
+        UserManager<IdentityUser> userManager,
+        RoleManager<IdentityRole> roleManager)
+    {
+        _logger = logger;
+        _context = context;
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+
+    public async Task InitialiseAsync()
+    {
+        try
+        {
+            if (_context.Database.IsSqlServer())
+            {
+                await _context.Database.MigrateAsync();
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while initialising the database.");
+            throw;
+        }
+    }
+
+    public async Task SeedAsync()
+    {
+        try
+        {
+            await TrySeedAsync();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error occurred while seeding the database.");
+            throw;
+        }
+    }
+
+    public async Task TrySeedAsync()
+    {
+        // Default roles
+        var administratorRole = new IdentityRole("Administrator");
+        if (_roleManager.Roles.All(r => r.Name != administratorRole.Name))
+        {
+            await _roleManager.CreateAsync(administratorRole);
+        }
+
+        // Default users
+        var administrator = new IdentityUser { UserName = "devadmin", Email = "devadmin@gmail.com" };
+        if (_userManager.Users.All(u => u.UserName != administrator.UserName))
+        {
+            var result = await _userManager.CreateAsync(administrator, "Devadmin@123");
+            if (result.Succeeded && administratorRole.Name != null)
+            {
+                await _userManager.AddToRolesAsync(administrator, new[] { administratorRole.Name });
+            }
+        }
+    }
+}
