ba95d3a87ffbf420094e7f9ff5547f7f58ca7452
Sitt Paing <sittp9288@gmail.com>
2026-08-02 17:28:25 +0630

    Update CategoryDto.cs

--- src/InventoryManagementSystem.Application/Categories/DTOs/CategoryDto.cs
+++ src/InventoryManagementSystem.Application/Categories/DTOs/CategoryDto.cs
@@ -1,8 +1,11 @@
 namespace InventoryManagementSystem.Application.Categories.DTOs;
 
-public record CategoryDto(
-    long Id,
-    string Name,
-    string? Description,
-    bool IsActive
-);
+public record CategoryDto
+{
+    public long Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime? CreatedOn { get; set; }
+    public DateTime? UpdatedOn { get; set; }
+}
