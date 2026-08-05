02fc8b577b52c6cda90c501da4aaa74b17addfd0
Sitt Paing <sittp9288@gmail.com>
2026-08-05 14:17:08 +0630

    Update CategoryDto.cs

--- src/InventoryManagementSystem.Application/Categories/DTOs/CategoryDto.cs
+++ src/InventoryManagementSystem.Application/Categories/DTOs/CategoryDto.cs
@@ -7,5 +7,9 @@ public record CategoryDto
     public string? Description { get; set; }
     public bool IsActive { get; set; }
     public DateTime? CreatedOn { get; set; }
+    public string? CreatedBy { get; set; }
     public DateTime? UpdatedOn { get; set; }
+    public string? UpdatedBy { get; set; }
+    public DateTime? DeletedOn { get; set; }
+    public string? DeletedBy { get; set; }
 }
