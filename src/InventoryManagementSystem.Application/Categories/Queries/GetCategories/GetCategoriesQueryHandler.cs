75a993fd104ac8d91b158aaaa8bf2547a1b7df45
Sitt Paing <sittp9288@gmail.com>
2026-08-05 16:33:22 +0630

    Create GetCategoriesQueryHandler.cs

---
+++ src/InventoryManagementSystem.Application/Categories/Queries/GetCategories/GetCategoriesQueryHandler.cs
@@ -0,0 +1,37 @@
+using InventoryManagementSystem.Application.Categories.DTOs;
+using InventoryManagementSystem.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagementSystem.Application.Categories.Queries.GetCategories;
+
+public class GetCategoriesQueryHandler : IRequestHandler<GetCategoriesQuery, List<CategoryDto>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetCategoriesQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<CategoryDto>> Handle(GetCategoriesQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.Categories
+            .AsNoTracking()
+            .Where(c => !c.DeletedOn.HasValue)
+            .Select(c => new CategoryDto
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Description = c.Description,
+                IsActive = c.IsActive,
+                CreatedOn = c.CreatedOn,
+                CreatedBy = c.CreatedBy,
+                UpdatedOn = c.UpdatedOn,
+                UpdatedBy = c.UpdatedBy,
+                DeletedOn = c.DeletedOn,
+                DeletedBy = c.DeletedBy
+            })
+            .ToListAsync(cancellationToken);
+    }
+}
