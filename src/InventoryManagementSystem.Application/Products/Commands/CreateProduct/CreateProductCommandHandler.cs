1d98b35d4df4ca1f00bbfd3a728eff24a0347a17
Sitt Paing <sittp9288@gmail.com>
2026-08-06 11:33:10 +0630

    Create CreateProductCommandHandler.cs

---
+++ src/InventoryManagementSystem.Application/Products/Commands/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using InventoryManagementSystem.Application.Common.Interfaces;
+using InventoryManagementSystem.Application.Products.DTOs;
+using MediatR;
+
+
+namespace InventoryManagementSystem.Application.Products.Commands.CreateProduct;
+
+public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, ProductDto>
+{
+    private readonly IApplicationDbContext _context;
+
+    public CreateProductCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<ProductDto> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        var entity = new Domain.Entities.Product
+        {
+            Id = request.Id,
+            Name = request.Name,
+            CategoryId = request.categoryId,
+            UnitPrice = request.UnitPrice,
+            Sku = request.Sku,
+            CurrentStock = request.CurrentStock,
+            ReorderLevel = request.ReorderLevel
+        };
+        _context.Products.Add(entity);
+        await _context.SaveChangesAsync(cancellationToken);
+        return new ProductDto
+        {
+            Id = entity.Id,
+            Name = entity.Name,
+            CategoryId = entity.CategoryId,
+            UnitPrice = entity.UnitPrice,
+            Sku = entity.Sku,
+            CurrentStock = entity.CurrentStock,
+            ReorderLevel = entity.ReorderLevel,
+            CreatedOn = DateTime.Now,
+            CreatedBy = entity.CreatedBy,
+        };
+    }
+}
