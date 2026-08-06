5cebd1bcd03d163ccf6a1f77572c9456655bc7e3
Sitt Paing <sittp9288@gmail.com>
2026-08-06 11:33:22 +0630

    Create GetProductsQuery.cs

---
+++ src/InventoryManagementSystem.Application/Products/Queries/GetProducts/GetProductsQuery.cs
@@ -0,0 +1,9 @@
+﻿using InventoryManagementSystem.Application.Products.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagementSystem.Application.Products.Queries.GetProducts;
+
+public record GetProductsQuery() : IRequest<List<ProductDto>>;
