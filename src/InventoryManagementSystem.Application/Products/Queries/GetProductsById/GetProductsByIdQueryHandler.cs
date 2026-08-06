c6d132f2d75b1abf9049afc132668a685250bfb7
Sitt Paing <sittp9288@gmail.com>
2026-08-06 11:33:33 +0630

    Create GetProductsByIdQueryHandler.cs

---
+++ src/InventoryManagementSystem.Application/Products/Queries/GetProductsById/GetProductsByIdQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using InventoryManagementSystem.Application.Common.Interfaces;
+using InventoryManagementSystem.Application.Products.DTOs;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagementSystem.Application.Products.Queries.GetProductsById;
+
+public class GetProductsByIdQueryHandler : IRequestHandler<GetProductByIdQuery, ProductDto?>
+{
+    private readonly IApplicationDbContext _context;
+    public GetProductsByIdQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<ProductDto?> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+    {
+        var product = await _context.Products
+            .AsNoTracking()
+            .Where(p => !p.DeletedOn.HasValue)
+            .FirstOrDefaultAsync(p => p.Id == request.ProductId, cancellationToken);
+        if (product == null) return null;
+        return new ProductDto
+        {
+            Id = product.Id,
+            Name = product.Name,
+            CurrentStock = product.CurrentStock,
+            ReorderLevel = product.ReorderLevel,
+            Sku = product.Sku,
+            UnitPrice = product.UnitPrice,
+            CategoryId = product.CategoryId,
+            CreatedOn = product.CreatedOn,
+            CreatedBy = product.CreatedBy,
+            UpdatedOn = product.UpdatedOn,
+            UpdatedBy = product.UpdatedBy,
+            DeletedOn = product.DeletedOn,
+            DeletedBy = product.DeletedBy
+        };
+    }
+}
