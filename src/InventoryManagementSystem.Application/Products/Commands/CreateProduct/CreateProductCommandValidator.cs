106bb2f3761baa828e252f99280fab71409aa1fb
Sitt Paing <sittp9288@gmail.com>
2026-08-06 11:33:14 +0630

    Create CreateProductCommandValidator.cs

---
+++ src/InventoryManagementSystem.Application/Products/Commands/CreateProduct/CreateProductCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagementSystem.Application.Products.Commands.CreateProduct;
+
+public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Product Id is required.");
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Product Name is required.");
+        RuleFor(x => x.categoryId).GreaterThan(0).WithMessage("Category Id must be greater").NotEmpty().WithMessage("Category Id is required.");
+        RuleFor(x => x.UnitPrice).GreaterThan(0).WithMessage("Unit Price must be greater than 0.").NotEmpty().WithMessage("Price is required.");
+        RuleFor(x => x.CurrentStock).GreaterThanOrEqualTo(0).WithMessage("Current Stock must be greater than or equal to 0.").NotEmpty().WithMessage("Current Stock is required.");
+        RuleFor(x => x.ReorderLevel).GreaterThanOrEqualTo(0).WithMessage("Reorder Level must be greater than or equal to 0.").NotEmpty().WithMessage("Reorder Level is required.");
+    }
+}
