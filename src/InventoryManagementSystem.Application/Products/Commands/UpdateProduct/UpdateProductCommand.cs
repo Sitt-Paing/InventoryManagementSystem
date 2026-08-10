59956a6a0fef36f115863e1224dd70bc36ea95de
Sitt Paing <sittp9288@gmail.com>
2026-08-10 15:04:31 +0630

    Update UpdateProductCommand.cs

--- src/InventoryManagementSystem.Application/Products/Commands/UpdateProduct/UpdateProductCommand.cs
+++ src/InventoryManagementSystem.Application/Products/Commands/UpdateProduct/UpdateProductCommand.cs
@@ -1,4 +1,4 @@
-﻿using InventoryManagementSystem.Application.Products.DTOs;
+using InventoryManagementSystem.Application.Products.DTOs;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -6,4 +6,4 @@ using System.Text;
 
 namespace InventoryManagementSystem.Application.Products.Commands.UpdateProduct;
 
-public record UpdateProductCommand(Guid Id,string Name,long CategoryId,string Sku,decimal UnitPrice, int CurrentStock, int ReorderLevel) : IRequest<ProductDto?>;
+public record UpdateProductCommand(Guid Id, string Name, long CategoryId, string Sku, decimal UnitPrice, int CurrentStock, int ReorderLevel, string? Barcode = null) : IRequest<ProductDto?>;
