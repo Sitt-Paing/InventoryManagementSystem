626fe63249fad4bf588ad4da3123c1eaf17f3d4f
Sitt Paing <sittp9288@gmail.com>
2026-08-10 11:38:31 +0630

    Create UpdateProductCommand.cs

---
+++ src/InventoryManagementSystem.Application/Products/Commands/UpdateProduct/UpdateProductCommand.cs
@@ -0,0 +1,9 @@
+﻿using InventoryManagementSystem.Application.Products.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagementSystem.Application.Products.Commands.UpdateProduct;
+
+public record UpdateProductCommand(Guid Id,string Name,long CategoryId,string Sku,decimal UnitPrice, int CurrentStock, int ReorderLevel) : IRequest<ProductDto?>;
