7771d847e0610cbe83de486d69a70947191da396
Sitt Paing <sittp9288@gmail.com>
2026-08-10 15:04:25 +0630

    Update CreateProductCommand.cs

--- src/InventoryManagementSystem.Application/Products/Commands/CreateProduct/CreateProductCommand.cs
+++ src/InventoryManagementSystem.Application/Products/Commands/CreateProduct/CreateProductCommand.cs
@@ -1,4 +1,4 @@
-﻿using InventoryManagementSystem.Application.Products.DTOs;
+using InventoryManagementSystem.Application.Products.DTOs;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -6,4 +6,4 @@ using System.Text;
 
 namespace InventoryManagementSystem.Application.Products.Commands.CreateProduct;
 
-public record CreateProductCommand(string Name, long categoryId, decimal UnitPrice, string? Sku, int CurrentStock, int ReorderLevel) : IRequest<ProductDto>;
+public record CreateProductCommand(string Name, long categoryId, decimal UnitPrice, string? Sku, int CurrentStock, int ReorderLevel, string? Barcode = null) : IRequest<ProductDto>;
