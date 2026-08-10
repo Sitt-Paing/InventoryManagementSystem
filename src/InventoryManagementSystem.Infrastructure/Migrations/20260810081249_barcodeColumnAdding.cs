44981a0f3c870fb6bb729b62499ad0bf5baef647
Sitt Paing <sittp9288@gmail.com>
2026-08-10 15:04:54 +0630

    Create 20260810081249_barcodeColumnAdding.cs

---
+++ src/InventoryManagementSystem.Infrastructure/Migrations/20260810081249_barcodeColumnAdding.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryManagementSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class barcodeColumnAdding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Barcode",
+                table: "Products",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Barcode",
+                table: "Products");
+        }
+    }
+}
