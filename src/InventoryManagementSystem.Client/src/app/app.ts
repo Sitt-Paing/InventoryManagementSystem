135db95671b6752383a9ed68765dd9718b13321f
Sitt Paing <sittp9288@gmail.com>
2026-08-11 14:55:21 +0630

    Update app.ts

--- src/InventoryManagementSystem.Client/src/app/app.ts
+++ src/InventoryManagementSystem.Client/src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { ToolbarModule } from 'primeng/toolbar';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, CardModule, ToolbarModule, TagModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('InventoryManagementSystem.Client');
+  protected readonly title = signal('Inventory Management System');
 }
