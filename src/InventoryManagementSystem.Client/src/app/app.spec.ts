ee9e90ac21683872fbe464840e308742acd7df3a
Sitt Paing <sittp9288@gmail.com>
2026-08-11 14:55:16 +0630

    Update app.spec.ts

--- src/InventoryManagementSystem.Client/src/app/app.spec.ts
+++ src/InventoryManagementSystem.Client/src/app/app.spec.ts
@@ -1,10 +1,21 @@
 import { TestBed } from '@angular/core/testing';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 import { App } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [
+        provideAnimationsAsync(),
+        providePrimeNG({
+          theme: {
+            preset: Aura
+          }
+        })
+      ]
     }).compileComponents();
   });
 
@@ -14,10 +25,10 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
+  it('should render title in header', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, InventoryManagementSystem.Client');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Inventory Management System');
   });
 });
