840cf2d24fe56730b2d45ed21e9c903a94e16989
Sitt Paing <sittp9288@gmail.com>
2026-08-12 11:38:42 +0630

    Create LoginUserCommand.cs

---
+++ src/InventoryManagementSystem.Application/Auth/Commands/LoginUser/LoginUserCommand.cs
@@ -0,0 +1,47 @@
+using FluentValidation;
+using InventoryManagementSystem.Application.Auth.Models;
+using InventoryManagementSystem.Application.Common.Interfaces;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Application.Auth.Commands.LoginUser;
+
+public record LoginUserCommand : IRequest<AuthResultDto>
+{
+    public string UserNameOrEmail { get; init; } = null!;
+    public string Password { get; init; } = null!;
+}
+
+public class LoginUserCommandValidator : AbstractValidator<LoginUserCommand>
+{
+    public LoginUserCommandValidator()
+    {
+        RuleFor(v => v.UserNameOrEmail)
+            .NotEmpty().WithMessage("Username or Email is required.");
+
+        RuleFor(v => v.Password)
+            .NotEmpty().WithMessage("Password is required.");
+    }
+}
+
+public class LoginUserCommandHandler : IRequestHandler<LoginUserCommand, AuthResultDto>
+{
+    private readonly IIdentityService _identityService;
+
+    public LoginUserCommandHandler(IIdentityService identityService)
+    {
+        _identityService = identityService;
+    }
+
+    public async Task<AuthResultDto> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+    {
+        var loginRequest = new LoginRequest
+        {
+            UserNameOrEmail = request.UserNameOrEmail,
+            Password = request.Password
+        };
+
+        return await _identityService.LoginAsync(loginRequest);
+    }
+}
