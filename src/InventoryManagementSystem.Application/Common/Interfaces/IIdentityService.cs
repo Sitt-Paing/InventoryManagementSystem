6dffffab25c1ca39f14c9e650abd7c8f3063f5f1
Sitt Paing <sittp9288@gmail.com>
2026-08-12 11:17:31 +0630

    Create IIdentityService.cs

---
+++ src/InventoryManagementSystem.Application/Common/Interfaces/IIdentityService.cs
@@ -0,0 +1,14 @@
+using InventoryManagementSystem.Application.Identity.Models;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Application.Common.Interfaces;
+
+public interface IIdentityService
+{
+    Task<AuthResultDto> RegisterAsync(RegisterRequest request);
+    Task<AuthResultDto> LoginAsync(LoginRequest request);
+    Task<AuthResultDto> RefreshTokenAsync(RefreshTokenRequest request);
+    Task<string?> GeneratePasswordResetTokenAsync(string email);
+    Task<ResultDto> ResetPasswordAsync(ResetPasswordRequest request);
+    Task<ResultDto> ChangePasswordAsync(string userId, ChangePasswordRequest request);
+}
