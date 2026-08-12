df1ddf5961ff19536942ecd301bea6196a63e7af
Sitt Paing <sittp9288@gmail.com>
2026-08-12 14:53:54 +0630

    Update app.routes.ts

--- src/InventoryManagementSystem.Client/src/app/app.routes.ts
+++ src/InventoryManagementSystem.Client/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { AppLayout } from './layout/app-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppLayout,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./pages/dashboard/dashboard').then(m => m.DashboardComponent)
+      },
+      {
+        path: 'products',
+        loadComponent: () => import('./pages/products/products').then(m => m.ProductsComponent)
+      },
+      {
+        path: 'categories',
+        loadComponent: () => import('./pages/categories/categories').then(m => m.CategoriesComponent)
+      },
+      {
+        path: 'stock-transactions',
+        loadComponent: () => import('./pages/stock-transactions/stock-transactions').then(m => m.StockTransactionsComponent)
+      }
+    ]
+  },
+  { path: '**', redirectTo: '' }
+];
