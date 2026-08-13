a86cf9ff5735f072f85838e1853c6a281292eaab
Sitt Paing <sittp9288@gmail.com>
2026-08-13 15:22:47 +0630

    Update category.model.ts

--- src/InventoryManagementSystem.Client/src/app/core/models/category.model.ts
+++ src/InventoryManagementSystem.Client/src/app/core/models/category.model.ts
@@ -1,10 +1,12 @@
 export interface CategoryModel {
-  categoryId: number;
-  categoryName: string;
-  categoryCode: string;
-  description?: string;
-  totalProducts?: number;
-  companyId: string;
-  branchId: number;
-  isActive: boolean;
+    id: number;
+    name: string;
+    description?: string | null;
+    isActive: boolean;
+    createdOn?: Date | null;
+    createdBy?: string | null;
+    updatedOn?: Date | null;
+    updatedBy?: string | null;
+    deletedOn?: Date | null;
+    deletedBy?: string | null;
 }
