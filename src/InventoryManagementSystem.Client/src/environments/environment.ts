82b2cb3a6214f1f53b4c3da0299c779c1ea8d54b
Sitt Paing <sittp9288@gmail.com>
2026-08-13 15:25:54 +0630

    Create environment.ts

---
+++ src/InventoryManagementSystem.Client/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  docker: false,
+
+  main_url: 'https://localhost:7152/api',
+  web_url: 'https://localhost:7152/images',
+
+  debug: true,
+
+  default_pagination: [10, 20, 30, 50, 100],
+
+  default_toastKey: "globalMessage",
+};
