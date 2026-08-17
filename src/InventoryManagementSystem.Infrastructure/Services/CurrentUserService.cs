943616e67d5dadbb63e602534035f44e7fb645ea
Sitt Paing <sittp9288@gmail.com>
2026-08-17 10:42:31 +0630

    Update CurrentUserService.cs

--- src/InventoryManagementSystem.Infrastructure/Services/CurrentUserService.cs
+++ src/InventoryManagementSystem.Infrastructure/Services/CurrentUserService.cs
@@ -14,7 +14,14 @@ public class CurrentUserService : ICurrentUserService
     }
 
     public string? UserId => _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier)
+                             ?? _httpContextAccessor.HttpContext?.User?.FindFirstValue("sub")
                              ?? _httpContextAccessor.HttpContext?.User?.Identity?.Name;
+
+    public string? UserName => _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.Name)
+                               ?? _httpContextAccessor.HttpContext?.User?.FindFirstValue("unique_name")
+                               ?? _httpContextAccessor.HttpContext?.User?.FindFirstValue("name")
+                               ?? _httpContextAccessor.HttpContext?.User?.FindFirstValue("username")
+                               ?? _httpContextAccessor.HttpContext?.User?.Identity?.Name;
 
     public string? IpAddress => _httpContextAccessor.HttpContext?.Connection?.RemoteIpAddress?.ToString();
 }
