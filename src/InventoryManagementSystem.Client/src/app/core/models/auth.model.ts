c7760b87e699e50a39da04b3c20cb33d904264c2
Sitt Paing <sittp9288@gmail.com>
2026-08-17 16:04:51 +0630

    Update auth.model.ts

--- src/InventoryManagementSystem.Client/src/app/core/models/auth.model.ts
+++ src/InventoryManagementSystem.Client/src/app/core/models/auth.model.ts
@@ -2,19 +2,25 @@ export interface LoginModel {
   usernameOrEmail: string;
   password: string;
   rememberMe?: boolean;
+  theme?: string;
+  language?: string;
 }
 
 export interface AuthResponseModel {
-  userId: string;
-  userName: string;
-  email: string;
-  roles: string[];
-  accessToken: string;
-  refreshToken: string;
-  expiresAt: string;
+  userId?: string;
+  userName?: string;
+  email?: string;
+  roles?: string[];
+  theme?: string;
+  language?: string;
+  rememberMe?: boolean;
+  accessToken?: string;
+  refreshToken?: string;
+  expiresAt?: string;
 }
 
 export interface RefreshTokenModel {
-  accessToken: string;
-  refreshToken: string;
+  accessToken?: string;
+  refreshToken?: string;
 }
+
