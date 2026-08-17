8d1be077f28651816c4e8d93d51fccda74837acd
Sitt Paing <sittp9288@gmail.com>
2026-08-17 16:04:34 +0630

    Create ICookieService.cs

---
+++ src/InventoryManagementSystem.Api/Services/ICookieService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace InventoryManagementSystem.Api.Services;
+
+public interface ICookieService
+{
+    void SetAuthCookies(string accessToken, string refreshToken, DateTime accessTokenExpiry, DateTime refreshTokenExpiry, bool rememberMe);
+    void ClearAuthCookies();
+    string? GetRefreshToken();
+    string? GetAccessToken();
+}
