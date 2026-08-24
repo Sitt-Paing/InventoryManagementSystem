456b60ecb1635e8d784e9efc9a7ee309e5c378b3
Sitt Paing <sittp9288@gmail.com>
2026-08-24 11:45:54 +0630

    Update IExportService.cs

--- src/InventoryManagementSystem.Application/Common/Interfaces/IExportService.cs
+++ src/InventoryManagementSystem.Application/Common/Interfaces/IExportService.cs
@@ -1,16 +1,30 @@
-using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
 
 namespace InventoryManagementSystem.Application.Common.Interfaces;
 
 public interface IExportService
 {
-    byte[] ExportToExcel<T>(IEnumerable<T> data, string sheetName = "Sheet1", string fontName = "Pyidaungsu");
-    
+    Stream? ExportToExcelStreamSpecificColumns<T>(
+        List<T>? list,
+        KeyValuePair<string, string>[] columns,
+        string sheetName = "Sheet1",
+        string fontName = "Pyidaungsu");
+
+    Stream? ExportToExcelStreamSpecificColumns<T>(
+        List<T>? list,
+        Dictionary<string, string> columns,
+        string sheetName = "Sheet1",
+        string fontName = "Pyidaungsu");
+
     byte[] ExportToExcel<T>(
         IEnumerable<T> data, 
         Dictionary<string, string> columnMappings, 
+        string sheetName = "Sheet1", 
+        string fontName = "Pyidaungsu");
+
+    byte[] ExportToExcel<T>(
+        IEnumerable<T> data, 
         string sheetName = "Sheet1", 
         string fontName = "Pyidaungsu");
 
