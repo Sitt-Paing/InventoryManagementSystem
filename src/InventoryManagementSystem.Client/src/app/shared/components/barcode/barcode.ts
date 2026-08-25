0d2ca436262f3f680935dd7f34d1553bf5401fb9
Sitt Paing <sittp9288@gmail.com>
2026-08-25 13:55:10 +0630

    Create barcode.ts

---
+++ src/InventoryManagementSystem.Client/src/app/shared/components/barcode/barcode.ts
@@ -0,0 +1,29 @@
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import JsBarcode from 'jsbarcode';
+
+@Component({
+  selector: 'app-barcode',
+  imports: [],
+  templateUrl: './barcode.html',
+  styleUrl: './barcode.scss',
+})
+export class Barcode implements AfterViewInit {
+  @Input() value: string | null = null;
+  @ViewChild('barcode', { static: true }) barcodeElement!: ElementRef<SVGSVGElement>;
+
+  ngAfterViewInit(): void {
+    this.generateBarcode();
+  }
+
+  generateBarcode(): void {
+    if(!this.value) return;
+
+    JsBarcode(this.barcodeElement.nativeElement, this.value, {
+      format: 'CODE128',
+      lineColor: '#000',
+      width: 2,
+      height: 100,
+      displayValue: true,
+    }); 
+  }
+}
