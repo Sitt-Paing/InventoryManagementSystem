c36ecea5cd02077ad0aae4bc7c89e38bdc734cd5
Sitt Paing <sittp9288@gmail.com>
2026-08-25 13:37:41 +0630

    Update CreateProductCommandValidator.cs

--- src/InventoryManagementSystem.Application/Products/Commands/CreateProduct/CreateProductCommandValidator.cs
+++ src/InventoryManagementSystem.Application/Products/Commands/CreateProduct/CreateProductCommandValidator.cs
@@ -13,6 +13,7 @@ public class CreateProductCommandValidator : AbstractValidator<CreateProductComm
         RuleFor(x => x.Name).NotEmpty().WithMessage("Product Name is required.");
         RuleFor(x => x.categoryId).GreaterThan(0).WithMessage("Category Id must be greater").NotEmpty().WithMessage("Category Id is required.");
         RuleFor(x => x.UnitPrice).GreaterThan(0).WithMessage("Unit Price must be greater than 0.").NotEmpty().WithMessage("Price is required.");
+        RuleFor(x => x.Barcode).MaximumLength(100).When(x => !string.IsNullOrWhiteSpace(x.Barcode)).WithMessage("Barcode must not exceed 100 characters");
         RuleFor(x => x.CurrentStock).GreaterThanOrEqualTo(0).WithMessage("Current Stock must be greater than or equal to 0.").NotEmpty().WithMessage("Current Stock is required.");
         RuleFor(x => x.ReorderLevel).GreaterThanOrEqualTo(0).WithMessage("Reorder Level must be greater than or equal to 0.").NotEmpty().WithMessage("Reorder Level is required.");
     }
