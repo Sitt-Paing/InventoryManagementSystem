1ab07044f05b8d5514d3fa7f79ede3a843c3fec4
Sitt Paing <sittp9288@gmail.com>
2026-08-26 15:01:08 +0630

    Create 20260826083038_FilterDeletedOnProductUniqueIndexes.cs

---
+++ src/InventoryManagementSystem.Infrastructure/Migrations/20260826083038_FilterDeletedOnProductUniqueIndexes.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryManagementSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FilterDeletedOnProductUniqueIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Products_Barcode",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_Sku",
+                table: "Products");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_Barcode",
+                table: "Products",
+                column: "Barcode",
+                unique: true,
+                filter: "[Barcode] IS NOT NULL AND [DeletedOn] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_Sku",
+                table: "Products",
+                column: "SKU",
+                unique: true,
+                filter: "[DeletedOn] IS NULL AND [SKU] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Products_Barcode",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_Sku",
+                table: "Products");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_Barcode",
+                table: "Products",
+                column: "Barcode",
+                unique: true,
+                filter: "[Barcode] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_Sku",
+                table: "Products",
+                column: "SKU",
+                unique: true,
+                filter: "[SKU] IS NOT NULL");
+        }
+    }
+}
