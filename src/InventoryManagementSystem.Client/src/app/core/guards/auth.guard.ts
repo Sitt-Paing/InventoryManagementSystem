32effc45f3e0e6a7052fa02339d41ff08e9d2b99
Sitt Paing <sittp9288@gmail.com>
2026-08-26 15:00:54 +0630

    Update auth.guard.ts

--- src/InventoryManagementSystem.Client/src/app/core/guards/auth.guard.ts
+++ src/InventoryManagementSystem.Client/src/app/core/guards/auth.guard.ts
@@ -2,7 +2,8 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = () => {
+
   const authService = inject(AuthService);
   const router = inject(Router);
 
@@ -10,6 +11,5 @@ export const authGuard: CanActivateFn = (route, state) => {
     return true;
   }
 
-  router.navigate(['/auth/login']);
-  return false;
+  return router.createUrlTree(['/auth/login']);
 };
