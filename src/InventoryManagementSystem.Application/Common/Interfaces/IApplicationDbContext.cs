5e578e9f4765dd310adc7864383dd2fb13e1037d
Sitt Paing <sittp9288@gmail.com>
2026-09-03 14:48:53 +0630

    Update IApplicationDbContext.cs

--- src/InventoryManagementSystem.Application/Common/Interfaces/IApplicationDbContext.cs
+++ src/InventoryManagementSystem.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -8,6 +8,9 @@ public interface IApplicationDbContext
     DbSet<Category> Categories { get; }
     DbSet<Product> Products { get; }
     DbSet<StockTransaction> StockTransactions { get; }
+    DbSet<Supplier> Suppliers { get; }
+    DbSet<Warehouse> Warehouses { get; }
+    DbSet<WarehouseLocation> WarehouseLocations { get; }
 
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
