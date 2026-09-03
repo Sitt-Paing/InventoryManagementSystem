0708498e182e5f53619e086f918b691da7f25d7d
Sitt Paing <sittp9288@gmail.com>
2026-09-03 14:31:53 +0630

    Create Warehouse.cs

---
+++ src/InventoryManagementSystem.Domain/Entities/Warehouse.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace InventoryManagementSystem.Domain.Entities;
+
+public partial class Warehouse
+{
+    public int Id { get; set; }
+
+    public string WarehouseCode { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string? Address { get; set; }
+
+    public string? ContactPerson { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? Email { get; set; }
+
+    public decimal? Capacity { get; set; }
+
+    public bool Status { get; set; }
+
+    public DateTime? CreatedOn { get; set; }
+
+    public string? CreatedBy { get; set; }
+
+    public DateTime? UpdatedOn { get; set; }
+
+    public string? UpdatedBy { get; set; }
+
+    public DateTime? DeletedOn { get; set; }
+
+    public string? DeletedBy { get; set; }
+
+    [JsonIgnore]
+    public virtual ICollection<WarehouseLocation> WarehouseLocations { get; set; } = new List<WarehouseLocation>();
+}
