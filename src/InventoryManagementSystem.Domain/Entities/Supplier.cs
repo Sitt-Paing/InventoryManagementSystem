1208c77aa1192853d2e1658d7a3d495200f78612
Sitt Paing <sittp9288@gmail.com>
2026-09-03 14:31:25 +0630

    Create Supplier.cs

---
+++ src/InventoryManagementSystem.Domain/Entities/Supplier.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InventoryManagementSystem.Domain.Entities;
+
+public partial class Supplier
+{
+    public int Id { get; set; }
+
+    public string SupplierCode { get; set; } = null!;
+
+    public string CompanyName { get; set; } = null!;
+
+    public string? ContactPerson { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Address { get; set; }
+
+    public string PaymentTerms { get; set; } = null!;
+
+    public decimal? CreditLimit { get; set; }
+
+    public bool Status { get; set; }
+
+    public DateTime? CreatedOn { get; set; }
+
+    public string? CreatedBy { get; set; }
+
+    public DateTime? UpdatedOn { get; set; }
+
+    public string? UpdatedBy { get; set; }
+
+    public DateTime? DeletedOn { get; set; }
+
+    public string? DeletedBy { get; set; }
+}
