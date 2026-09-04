95db030fb026950cb850d1a9ceb3727114f457e5
Sitt Paing <sittp9288@gmail.com>
2026-09-04 11:18:45 +0630

    Create UpdateSupplierCommandValidator.cs

---
+++ src/InventoryManagementSystem.Application/Suppliers/Commands/UpdateSupplier/UpdateSupplierCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagementSystem.Application.Suppliers.Commands.UpdateSupplier;
+
+public class UpdateSupplierCommandValidator : AbstractValidator<UpdateSupplierCommand>
+{
+    public UpdateSupplierCommandValidator()
+    {
+        RuleFor(x => x.id).GreaterThan(0).WithMessage("Supplier ID must be greater than 0.");
+        RuleFor(x => x.supplierCode).NotEmpty().WithMessage("Supplier code is required.");
+        RuleFor(x => x.companyName).NotEmpty().WithMessage("Company name is required.");
+        RuleFor(x => x.paymentTerms).NotEmpty().WithMessage("Payment terms are required.");
+        RuleFor(x => x.creditLimit).GreaterThanOrEqualTo(0).WithMessage("Credit limit must be greater than or equal to 0.");
+    }
+}
