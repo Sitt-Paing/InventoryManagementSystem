a54942c9e31e3b185762c5a4d3a78e67ee340a96
Sitt Paing <sittp9288@gmail.com>
2026-09-04 09:36:59 +0630

    Create SupplierDto.cs

---
+++ src/InventoryManagementSystem.Application/Suppliers/DTOs/SupplierDto.cs
@@ -0,0 +1,41 @@
+﻿using InventoryManagementSystem.Domain.Common;
+using InventoryManagementSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagementSystem.Application.Suppliers.DTOs;
+
+public record SupplierDto
+{
+    public int Id { get; set; }
+    public string SupplierCode { get; set; } = null!;
+
+    public string CompanyName { get; set; } = null!;
+
+    public string? ContactPerson { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Address { get; set; }
+
+    public string PaymentTerms { get; set; } = null!;
+
+    public decimal? CreditLimit { get; set; }
+
+    public bool Status { get; set; }
+
+    public DateTime? CreatedOn { get; set; }
+
+    public string? CreatedBy { get; set; }
+
+    public DateTime? UpdatedOn { get; set; }
+
+    public string? UpdatedBy { get; set; }
+
+    public DateTime? DeletedOn { get; set; }
+
+    public string? DeletedBy { get; set; }
+}
