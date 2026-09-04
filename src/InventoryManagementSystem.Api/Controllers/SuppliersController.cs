2269cff9148fcb4c02fd2a1e4d865848482c3282
Sitt Paing <sittp9288@gmail.com>
2026-09-04 11:18:28 +0630

    Update SuppliersController.cs

--- src/InventoryManagementSystem.Api/Controllers/SuppliersController.cs
+++ src/InventoryManagementSystem.Api/Controllers/SuppliersController.cs
@@ -1,5 +1,7 @@
 ﻿using InventoryManagementSystem.Application.Common.Models;
 using InventoryManagementSystem.Application.Suppliers.Commands.CreateSupplier;
+using InventoryManagementSystem.Application.Suppliers.Commands.DeleteSupplier;
+using InventoryManagementSystem.Application.Suppliers.Commands.UpdateSupplier;
 using InventoryManagementSystem.Application.Suppliers.DTOs;
 using InventoryManagementSystem.Application.Suppliers.Queries.GetSuppliers;
 using Microsoft.AspNetCore.Http;
@@ -23,7 +25,7 @@ namespace InventoryManagementSystem.Api.Controllers
                 Message = "Suppliers retrieved successfully.",
                 Data = suppliers
             });
-        } 
+        }
 
         [HttpPost]
         [EndpointSummary("Create a new supplier")]
@@ -35,6 +37,57 @@ namespace InventoryManagementSystem.Api.Controllers
                 StatusCode = StatusCodes.Status201Created,
                 Success = true,
                 Message = "Supplier created successfully.",
+                Data = result
+            });
+        }
+
+        [HttpPut("{id}")]
+        [EndpointSummary("Update an existing supplier")]
+        public async Task<IActionResult> UpdateSupplier(int id, UpdateSupplierCommand command)
+        {
+            if (id != command.id)
+            {
+                return BadRequest(new DefaultResponseModel
+                {
+                    StatusCode = StatusCodes.Status400BadRequest,
+                    Success = false,
+                    Message = "Supplier ID mismatch.",
+                    Data = null
+                });
+            }
+            SupplierDto? result = await Mediator.Send(command);
+            if (result == null) return NotFound();
+            return Ok(new DefaultResponseModel
+            {
+                StatusCode = StatusCodes.Status200OK,
+                Success = true,
+                Message = "Supplier updated successfully.",
+                Data = result
+            });
+        }
+
+        [HttpDelete("{id}")]
+        [EndpointSummary("Delete a supplier")]
+        public async Task<IActionResult> DeleteSupplier(int id)
+        {
+            if(id <= 0)
+            {
+                return BadRequest(new DefaultResponseModel
+                {
+                    StatusCode = StatusCodes.Status400BadRequest,
+                    Success = false,
+                    Message = "Invalid supplier ID.",
+                    Data = null
+                });
+            }
+
+            SupplierDto result = await Mediator.Send(new DeleteSupplierCommand(id));
+            if (result == null) return NotFound();
+            return Ok(new DefaultResponseModel
+            {
+                StatusCode = StatusCodes.Status200OK,
+                Success = true,
+                Message = "Supplier deleted successfully.",
                 Data = result
             });
         }
