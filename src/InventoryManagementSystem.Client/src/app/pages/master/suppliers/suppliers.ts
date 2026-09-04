57b58dc1d268c40d699d9ee022602120ea26cc67
Sitt Paing <sittp9288@gmail.com>
2026-09-04 16:07:13 +0630

    Create suppliers.ts

---
+++ src/InventoryManagementSystem.Client/src/app/pages/master/suppliers/suppliers.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-suppliers',
+  imports: [],
+  templateUrl: './suppliers.html',
+  styleUrl: './suppliers.scss',
+})
+export class Suppliers implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
