c772480cf9b285f4a2d70c5ab63194fe608ccab8
Sitt Paing <sittp9288@gmail.com>
2026-09-04 14:45:17 +0630

    Create GetSupplierByIdCommand.cs

---
+++ src/InventoryManagementSystem.Application/Suppliers/Queries/GetSupplierById/GetSupplierByIdCommand.cs
@@ -0,0 +1,9 @@
+﻿using InventoryManagementSystem.Application.Suppliers.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagementSystem.Application.Suppliers.Queries.GetSupplierById;
+
+public record GetSupplierByIdQuery(int Id) : IRequest<SupplierDto?>;
