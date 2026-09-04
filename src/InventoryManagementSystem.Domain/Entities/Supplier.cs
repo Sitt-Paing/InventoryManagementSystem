83f44697dede627a2241ed848c6e211d3d0e5253
Sitt Paing <sittp9288@gmail.com>
2026-09-04 09:37:11 +0630

    Update Supplier.cs

--- src/InventoryManagementSystem.Domain/Entities/Supplier.cs
+++ src/InventoryManagementSystem.Domain/Entities/Supplier.cs
@@ -1,12 +1,11 @@
-﻿using System;
+﻿using InventoryManagementSystem.Domain.Common;
+using System;
 using System.Collections.Generic;
 
 namespace InventoryManagementSystem.Domain.Entities;
 
-public partial class Supplier
+public partial class Supplier : BaseAuditableEntity<int>
 {
-    public int Id { get; set; }
-
     public string SupplierCode { get; set; } = null!;
 
     public string CompanyName { get; set; } = null!;
@@ -24,16 +23,4 @@ public partial class Supplier
     public decimal? CreditLimit { get; set; }
 
     public bool Status { get; set; }
-
-    public DateTime? CreatedOn { get; set; }
-
-    public string? CreatedBy { get; set; }
-
-    public DateTime? UpdatedOn { get; set; }
-
-    public string? UpdatedBy { get; set; }
-
-    public DateTime? DeletedOn { get; set; }
-
-    public string? DeletedBy { get; set; }
 }
