cbd283e208cf1e4300f8a92d3928294c053607a9
Sitt Paing <sittp9288@gmail.com>
2026-09-04 09:37:08 +0630

    Create GetSuppliersQueryHandler.cs

---
+++ src/InventoryManagementSystem.Application/Suppliers/Queries/GetSuppliers/GetSuppliersQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using InventoryManagementSystem.Application.Common.Interfaces;
+using InventoryManagementSystem.Application.Suppliers.DTOs;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagementSystem.Application.Suppliers.Queries.GetSuppliers;
+
+public class GetSuppliersQueryHandler : IRequestHandler<GetSuppliersQuery, List<SupplierDto>>
+{
+    private readonly IApplicationDbContext context;
+    public GetSuppliersQueryHandler(IApplicationDbContext _context)
+    {
+        this.context = _context;
+    }
+
+    public async Task<List<SupplierDto>> Handle(GetSuppliersQuery request, CancellationToken cancellationToken)
+    {
+        var suppliers = await context.Suppliers.AsNoTracking().ToListAsync(cancellationToken);
+        return suppliers.Select(s => new SupplierDto
+        {
+            Id = s.Id,
+            SupplierCode = s.SupplierCode,
+            CompanyName = s.CompanyName,
+            ContactPerson = s.ContactPerson,
+            Phone = s.Phone,
+            Email = s.Email,
+            Address = s.Address,
+            PaymentTerms = s.PaymentTerms,
+            CreditLimit = s.CreditLimit,
+            Status = s.Status
+        }).ToList();
+    }
+}
