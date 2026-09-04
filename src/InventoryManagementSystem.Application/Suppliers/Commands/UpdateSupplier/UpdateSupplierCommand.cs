1fd2da25d3f680dcaef11deee0146c919d5a7c0c
Sitt Paing <sittp9288@gmail.com>
2026-09-04 11:18:38 +0630

    Create UpdateSupplierCommand.cs

---
+++ src/InventoryManagementSystem.Application/Suppliers/Commands/UpdateSupplier/UpdateSupplierCommand.cs
@@ -0,0 +1,9 @@
+﻿using InventoryManagementSystem.Application.Suppliers.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagementSystem.Application.Suppliers.Commands.UpdateSupplier;
+
+public record UpdateSupplierCommand(int supplierId, string supplierCode, string companyName, string? contactPerson, string? phone, string? email, string? address, string paymentTerms, decimal? creditLimit, bool status) : IRequest<SupplierDto>;
