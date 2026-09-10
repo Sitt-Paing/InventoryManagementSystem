595d940977d29c05ce4301202810f874fc3e6c5f
Sitt Paing <sittp9288@gmail.com>
2026-09-10 10:19:14 +0630

    Create CreateWarehouseCommandValidator.cs

---
+++ src/InventoryManagementSystem.Application/Warehouses/Commands/CreateWarehouse/CreateWarehouseCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using InventoryManagementSystem.Application.Warehouses.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagementSystem.Application.Warehouses.Commands.CreateWarehouse;
+
+public class CreateWarehouseCommandValidator : AbstractValidator<CreateWarehouseCommand>
+{
+    public CreateWarehouseCommandValidator()
+    {
+        RuleFor(x => x.WarehouseCode).NotEmpty().WithMessage("WarehouseCode is required!")
+            .MaximumLength(50).WithMessage("Warehouse code cannot exceed 50 characters.");
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Warehouse Name is required!")
+            .MaximumLength(200).WithMessage("Warehouse name cannot exceed 50 characters.");
+        RuleFor(x => x.ContactPerson)
+             .MaximumLength(100).WithMessage("Contact person cannot exceed 100 characters.");
+        RuleFor(x => x.Phone)
+            .MaximumLength(20).WithMessage("Phone number cannot exceed 20 characters.");
+        RuleFor(x => x.Email)
+            .EmailAddress().WithMessage("Invalid email address format.")
+            .MaximumLength(100).WithMessage("Email address cannot exceed 100 characters.");
+        RuleFor(x => x.Address)
+            .MaximumLength(200).WithMessage("Address cannot exceed 200 characters.");
+    }
+}
