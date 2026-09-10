8d7308ad3c23b9e2c802a63ae7a173c6f6f80c1e
Sitt Paing <sittp9288@gmail.com>
2026-09-10 10:19:11 +0630

    Create CreateWarehouseCommand.cs

---
+++ src/InventoryManagementSystem.Application/Warehouses/Commands/CreateWarehouse/CreateWarehouseCommand.cs
@@ -0,0 +1,9 @@
+﻿using InventoryManagementSystem.Application.Warehouses.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagementSystem.Application.Warehouses.Commands.CreateWarehouse;
+
+public record class CreateWarehouseCommand(int Id, string WarehouseCode, string Name,string? Address, string? ContactPerson, string? Phone, string? Email,string? capacity, bool status) : IRequest<WarehouseDto>;
