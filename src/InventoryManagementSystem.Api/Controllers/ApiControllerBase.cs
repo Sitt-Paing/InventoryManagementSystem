1708b0eeced7feaa02eb3797afffc2e26d5143ad
Sitt Paing <sittp9288@gmail.com>
2026-09-14 15:51:41 +0630

    Update ApiControllerBase.cs

--- src/InventoryManagementSystem.Api/Controllers/ApiControllerBase.cs
+++ src/InventoryManagementSystem.Api/Controllers/ApiControllerBase.cs
@@ -11,7 +11,9 @@ public abstract class ApiControllerBase : ControllerBase
 {
     private ISender? _mediator;
     private IExportService? _exportService;
+    private IBarcodeGenerationService? _barcodeGenerationService;
 
     protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
     protected IExportService ExportService => _exportService ??= HttpContext.RequestServices.GetRequiredService<IExportService>();
+    protected IBarcodeGenerationService BarcodeGenerationService => _barcodeGenerationService ??= HttpContext.RequestServices.GetRequiredService<IBarcodeGenerationService>();
 }
