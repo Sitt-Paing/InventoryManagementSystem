e321d2692e8df28b6fcab7b97c047b63ae42f15f
Sitt Paing <sittp9288@gmail.com>
2026-09-16 15:21:50 +0630

    Create UomCategoryDto.cs

---
+++ src/InventoryManagementSystem.Application/UomCategories/DTOs/UomCategoryDto.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace InventoryManagementSystem.Application.UomCategories.DTOs;
+
+public record UomCategoryDto
+{
+    public long Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime? CreatedOn { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? UpdatedOn { get; set; }
+    public string? UpdatedBy { get; set; }
+    public DateTime? DeletedOn { get; set; }
+    public string? DeletedBy { get; set; }
+}
