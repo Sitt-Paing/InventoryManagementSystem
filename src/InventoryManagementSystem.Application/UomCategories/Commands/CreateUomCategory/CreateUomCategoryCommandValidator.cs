b4985f4d0ce487ff946a314bb02dc84fd30ae9fd
Sitt Paing <sittp9288@gmail.com>
2026-09-16 15:21:32 +0630

    Create CreateUomCategoryCommandValidator.cs

---
+++ src/InventoryManagementSystem.Application/UomCategories/Commands/CreateUomCategory/CreateUomCategoryCommandValidator.cs
@@ -0,0 +1,16 @@
+using FluentValidation;
+
+namespace InventoryManagementSystem.Application.UomCategories.Commands.CreateUomCategory;
+
+public class CreateUomCategoryCommandValidator : AbstractValidator<CreateUomCategoryCommand>
+{
+    public CreateUomCategoryCommandValidator()
+    {
+        RuleFor(v => v.Name)
+            .NotEmpty().WithMessage("UOM Category Name is required.")
+            .MaximumLength(100).WithMessage("UOM Category Name must not exceed 100 characters.");
+
+        RuleFor(v => v.Description)
+            .MaximumLength(250).WithMessage("Description must not exceed 250 characters.");
+    }
+}
