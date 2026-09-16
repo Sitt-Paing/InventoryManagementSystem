8f88777d6a38ed8da085144785e3bccee23b0592
Sitt Paing <sittp9288@gmail.com>
2026-09-16 15:21:56 +0630

    Create GetUomCategoriesQueryHandler.cs

---
+++ src/InventoryManagementSystem.Application/UomCategories/Queries/GetUomCategories/GetUomCategoriesQueryHandler.cs
@@ -0,0 +1,41 @@
+using InventoryManagementSystem.Application.Common.Interfaces;
+using InventoryManagementSystem.Application.UomCategories.DTOs;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Application.UomCategories.Queries.GetUomCategories;
+
+public class GetUomCategoriesQueryHandler : IRequestHandler<GetUomCategoriesQuery, List<UomCategoryDto>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetUomCategoriesQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<UomCategoryDto>> Handle(GetUomCategoriesQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.UomCategories
+            .AsNoTracking()
+            .Where(c => !c.DeletedOn.HasValue)
+            .Select(c => new UomCategoryDto
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Description = c.Description,
+                IsActive = c.IsActive,
+                CreatedOn = c.CreatedOn,
+                CreatedBy = c.CreatedBy,
+                UpdatedOn = c.UpdatedOn,
+                UpdatedBy = c.UpdatedBy,
+                DeletedOn = c.DeletedOn,
+                DeletedBy = c.DeletedBy
+            })
+            .ToListAsync(cancellationToken);
+    }
+}
