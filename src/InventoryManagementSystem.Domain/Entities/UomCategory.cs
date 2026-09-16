33400311bba3d816e53fe2a4087b7f9f4c4e3818
Sitt Paing <sittp9288@gmail.com>
2026-09-16 11:59:01 +0630

    Create UomCategory.cs

---
+++ src/InventoryManagementSystem.Domain/Entities/UomCategory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InventoryManagementSystem.Domain.Entities;
+
+public partial class UomCategory
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public DateTime? CreatedOn { get; set; }
+
+    public string? CreatedBy { get; set; }
+
+    public DateTime? UpdatedOn { get; set; }
+
+    public string? UpdatedBy { get; set; }
+
+    public DateTime? DeletedOn { get; set; }
+
+    public string? DeletedBy { get; set; }
+
+    public virtual ICollection<UnitOfMeasure> UnitOfMeasures { get; set; } = new List<UnitOfMeasure>();
+}
