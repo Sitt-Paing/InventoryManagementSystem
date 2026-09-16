5c782608d750b737cbc932571f876dbd04663afc
Sitt Paing <sittp9288@gmail.com>
2026-09-16 11:59:04 +0630

    Create UnitOfMeasure.cs

---
+++ src/InventoryManagementSystem.Domain/Entities/UnitOfMeasure.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InventoryManagementSystem.Domain.Entities;
+
+public partial class UnitOfMeasure
+{
+    public long Id { get; set; }
+
+    public long CategoryId { get; set; }
+
+    public string Code { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string? Symbol { get; set; }
+
+    public int DecimalPlaces { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public DateTime? CreatedOn { get; set; }
+
+    public string? CreatedBy { get; set; }
+
+    public DateTime? UpdatedOn { get; set; }
+
+    public string? UpdatedBy { get; set; }
+
+    public DateTime? DeletedOn { get; set; }
+
+    public string? DeletedBy { get; set; }
+
+    public virtual UomCategory Category { get; set; } = null!;
+
+    public virtual ICollection<ProductUomConversion> ProductUomConversionFromUoms { get; set; } = new List<ProductUomConversion>();
+
+    public virtual ICollection<ProductUomConversion> ProductUomConversionToUoms { get; set; } = new List<ProductUomConversion>();
+}
