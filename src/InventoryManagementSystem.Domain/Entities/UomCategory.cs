e540c37503ec8e8b1d3a7ac86546ad7acd73103d
Sitt Paing <sittp9288@gmail.com>
2026-09-16 15:23:32 +0630

    Update UomCategory.cs

--- src/InventoryManagementSystem.Domain/Entities/UomCategory.cs
+++ src/InventoryManagementSystem.Domain/Entities/UomCategory.cs
@@ -1,30 +1,16 @@
-﻿using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
+using InventoryManagementSystem.Domain.Common;
 
 namespace InventoryManagementSystem.Domain.Entities;
 
-public partial class UomCategory
+public partial class UomCategory : BaseAuditableEntity<long>
 {
-    public long Id { get; set; }
-
     public string Name { get; set; } = null!;
 
     public string? Description { get; set; }
 
-    public bool IsActive { get; set; }
-
-    public DateTime? CreatedOn { get; set; }
-
-    public string? CreatedBy { get; set; }
-
-    public DateTime? UpdatedOn { get; set; }
-
-    public string? UpdatedBy { get; set; }
-
-    public DateTime? DeletedOn { get; set; }
-
-    public string? DeletedBy { get; set; }
+    public bool IsActive { get; set; } = true;
 
     [JsonIgnore]
     public virtual ICollection<UnitOfMeasure> UnitOfMeasures { get; set; } = new List<UnitOfMeasure>();
