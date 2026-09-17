724016708d1ff99757025f49b428f6aedd43d45f
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:13:12 +0630

    Delete DeleteProductCommand.cs

--- src/InventoryManagementSystem.Application/Products/Commands/DeleteProduct/DeleteProductCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using InventoryManagementSystem.Application.Products.DTOs;
-using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace InventoryManagementSystem.Application.Products.Commands.DeleteProduct;
-
-public record DeleteProductCommand(Guid Id) : IRequest<ProductDto?>;
