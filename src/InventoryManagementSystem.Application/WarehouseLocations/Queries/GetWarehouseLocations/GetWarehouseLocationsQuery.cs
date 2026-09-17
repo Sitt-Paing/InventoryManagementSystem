7efc28ad961dd74a4ac3920e9cc1dd0a24feb6cd
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:13:36 +0630

    Delete GetWarehouseLocationsQuery.cs

--- src/InventoryManagementSystem.Application/WarehouseLocations/Queries/GetWarehouseLocations/GetWarehouseLocationsQuery.cs
+++
@@ -1,7 +0,0 @@
-using InventoryManagementSystem.Application.WarehouseLocations.DTOs;
-using MediatR;
-using System.Collections.Generic;
-
-namespace InventoryManagementSystem.Application.WarehouseLocations.Queries.GetWarehouseLocations;
-
-public record class GetWarehouseLocationsQuery(int? WarehouseId = null) : IRequest<List<WarehouseLocationDto>>;
