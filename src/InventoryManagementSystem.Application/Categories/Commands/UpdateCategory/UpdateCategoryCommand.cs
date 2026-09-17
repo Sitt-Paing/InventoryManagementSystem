ebfdae76364072384e6bc3241c2453bc8afbb49a
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:07:06 +0630

    Delete UpdateCategoryCommand.cs

--- src/InventoryManagementSystem.Application/Categories/Commands/UpdateCategory/UpdateCategoryCommand.cs
+++
@@ -1,6 +0,0 @@
-using InventoryManagementSystem.Application.Categories.DTOs;
-using MediatR;
-
-namespace InventoryManagementSystem.Application.Categories.Commands.UpdateCategory;
-
-public record UpdateCategoryCommand(long Id, string Name, string? Description, bool IsActive) : IRequest<CategoryDto?>;
