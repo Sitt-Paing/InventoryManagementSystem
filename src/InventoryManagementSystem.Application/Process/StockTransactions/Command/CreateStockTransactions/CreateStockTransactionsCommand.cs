336733d75bc39a3ee39708d35034b3ab5c29220e
Sitt Paing <sittp9288@gmail.com>
2026-09-17 14:40:14 +0630

    Create CreateStockTransactionsCommand.cs

---
+++ src/InventoryManagementSystem.Application/Process/StockTransactions/Command/CreateStockTransactions/CreateStockTransactionsCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagementSystem.Application.Process.StockTransactions.Command.CreateStockTransactions
+{
+    internal class CreateStockTransactionsCommand
+    {
+    }
+}
