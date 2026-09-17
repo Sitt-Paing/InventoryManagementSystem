5d2292f4692ccaddc8fb3c901e3ac80476e8a603
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:10:19 +0630

    Create GetProductsByIdQuery.cs

---
+++ src/InventoryManagementSystem.Application/Master/Products/Queries/GetProductsById/GetProductsByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using InventoryManagementSystem.Application.Master.Products.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagementSystem.Application.Master.Products.Queries.GetProductsById;
+
+public record GetProductByIdQuery(Guid ProductId) : IRequest<ProductDto>;
