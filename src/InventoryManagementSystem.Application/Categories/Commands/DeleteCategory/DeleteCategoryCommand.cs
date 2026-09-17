2ace0324a20c74683f208fb11074e69c170478f9
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:07:09 +0630

    Delete DeleteCategoryCommand.cs

--- src/InventoryManagementSystem.Application/Categories/Commands/DeleteCategory/DeleteCategoryCommand.cs
+++
@@ -1,6 +0,0 @@
-using InventoryManagementSystem.Application.Categories.DTOs;
-using MediatR;
-
-namespace InventoryManagementSystem.Application.Categories.Commands.DeleteCategory;
-
-public record DeleteCategoryCommand(long Id) : IRequest<CategoryDto?>;
