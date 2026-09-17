6f65021607342e77afb76a0ad1ca79005de0882e
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:13:31 +0630

    Delete GetUomCategoryByIdQuery.cs

--- src/InventoryManagementSystem.Application/UomCategories/Queries/GetUomCategoryById/GetUomCategoryByIdQuery.cs
+++
@@ -1,6 +0,0 @@
-using InventoryManagementSystem.Application.UomCategories.DTOs;
-using MediatR;
-
-namespace InventoryManagementSystem.Application.UomCategories.Queries.GetUomCategoryById;
-
-public record GetUomCategoryByIdQuery(long Id) : IRequest<UomCategoryDto?>;
