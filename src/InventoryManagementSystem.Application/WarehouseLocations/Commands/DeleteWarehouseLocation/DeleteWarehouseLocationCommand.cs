a736c036b56f395870765bb61ed8d590ca9d932d
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:13:33 +0630

    Delete DeleteWarehouseLocationCommand.cs

--- src/InventoryManagementSystem.Application/WarehouseLocations/Commands/DeleteWarehouseLocation/DeleteWarehouseLocationCommand.cs
+++
@@ -1,6 +0,0 @@
-using InventoryManagementSystem.Application.WarehouseLocations.DTOs;
-using MediatR;
-
-namespace InventoryManagementSystem.Application.WarehouseLocations.Commands.DeleteWarehouseLocation;
-
-public record class DeleteWarehouseLocationCommand(int Id) : IRequest<WarehouseLocationDto?>;
