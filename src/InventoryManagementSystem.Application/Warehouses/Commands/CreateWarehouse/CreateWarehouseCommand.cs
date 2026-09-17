04921848edda082428e10deb084e20e45af4fe1f
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:13:37 +0630

    Delete CreateWarehouseCommand.cs

--- src/InventoryManagementSystem.Application/Warehouses/Commands/CreateWarehouse/CreateWarehouseCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using InventoryManagementSystem.Application.Warehouses.DTOs;
-using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace InventoryManagementSystem.Application.Warehouses.Commands.CreateWarehouse;
-
-public record class CreateWarehouseCommand(int Id, string WarehouseCode, string Name,string? Address, string? ContactPerson, string? Phone, string? Email,decimal? Capacity, bool Status) : IRequest<WarehouseDto>;
