8ef135242fc994ff55eeab9cad0ac775af09c294
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:13:23 +0630

    Delete DeleteUnitOfMeasureCommand.cs

--- src/InventoryManagementSystem.Application/UnitOfMeasures/Commands/DeleteUnitOfMeasure/DeleteUnitOfMeasureCommand.cs
+++
@@ -1,6 +0,0 @@
-using InventoryManagementSystem.Application.UnitOfMeasures.DTOs;
-using MediatR;
-
-namespace InventoryManagementSystem.Application.UnitOfMeasures.Commands.DeleteUnitOfMeasure;
-
-public record DeleteUnitOfMeasureCommand(long Id) : IRequest<UnitOfMeasureDto?>;
