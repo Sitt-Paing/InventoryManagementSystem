16b8a6993fcfcf8c5a5c468e1df424fc4d37fa57
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:14:13 +0630

    Add CreateWarehouseCommand.cs

---
+++ src/InventoryManagementSystem.Application/Master/Warehouses/Commands/CreateWarehouse/CreateWarehouseCommand.cs
@@ -0,0 +1,9 @@
+﻿using InventoryManagementSystem.Application.Master.Warehouses.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagementSystem.Application.Master.Warehouses.Commands.CreateWarehouse;
+
+public record class CreateWarehouseCommand(int Id, string WarehouseCode, string Name,string? Address, string? ContactPerson, string? Phone, string? Email,decimal? Capacity, bool Status) : IRequest<WarehouseDto>;
