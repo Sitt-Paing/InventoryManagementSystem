89e60a240658fa06a5f2ba8f51b73b07a92c186c
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:13:20 +0630

    Delete GetSupplierByIdCommand.cs

--- src/InventoryManagementSystem.Application/Suppliers/Queries/GetSupplierById/GetSupplierByIdCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using InventoryManagementSystem.Application.Suppliers.DTOs;
-using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace InventoryManagementSystem.Application.Suppliers.Queries.GetSupplierById;
-
-public record GetSupplierByIdQuery(int Id) : IRequest<SupplierDto?>;
