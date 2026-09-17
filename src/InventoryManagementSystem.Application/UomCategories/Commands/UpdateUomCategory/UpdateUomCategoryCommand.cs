babd0bf5cd29c2552beee2146da7d23d51325d47
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:13:29 +0630

    Delete UpdateUomCategoryCommand.cs

--- src/InventoryManagementSystem.Application/UomCategories/Commands/UpdateUomCategory/UpdateUomCategoryCommand.cs
+++
@@ -1,6 +0,0 @@
-using InventoryManagementSystem.Application.UomCategories.DTOs;
-using MediatR;
-
-namespace InventoryManagementSystem.Application.UomCategories.Commands.UpdateUomCategory;
-
-public record UpdateUomCategoryCommand(long Id, string Name, string? Description, bool IsActive) : IRequest<UomCategoryDto?>;
