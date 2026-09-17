cc6ab966688e66364dc0c64a92ba9b6977d7e63e
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:07:00 +0630

    Delete CreateCategoryCommand.cs

--- src/InventoryManagementSystem.Application/Categories/Commands/CreateCategory/CreateCategoryCommand.cs
+++
@@ -1,6 +0,0 @@
-using InventoryManagementSystem.Application.Categories.DTOs;
-using MediatR;
-
-namespace InventoryManagementSystem.Application.Categories.Commands.CreateCategory;
-
-public record CreateCategoryCommand(string Name, string? Description) : IRequest<CategoryDto>;
