a2c8b6bfd3d6c07936319a50916f649a674bb1a2
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:08:18 +0630

    Delete GetCategoryByIdQuery.cs

--- src/InventoryManagementSystem.Application/Categories/Queries/GetCategoryById/GetCategoryByIdQuery.cs
+++
@@ -1,6 +0,0 @@
-using InventoryManagementSystem.Application.Categories.DTOs;
-using MediatR;
-
-namespace InventoryManagementSystem.Application.Categories.Queries.GetCategoryById;
-
-public record GetCategoryByIdQuery(long Id) : IRequest<CategoryDto?>;
