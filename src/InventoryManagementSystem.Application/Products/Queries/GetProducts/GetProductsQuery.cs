36913cc9429d99044b8b2991340c1743d756e74f
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:13:15 +0630

    Delete GetProductsQuery.cs

--- src/InventoryManagementSystem.Application/Products/Queries/GetProducts/GetProductsQuery.cs
+++
@@ -1,9 +0,0 @@
-using InventoryManagementSystem.Application.Products.DTOs;
-using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace InventoryManagementSystem.Application.Products.Queries.GetProducts;
-
-public record GetProductsQuery(long? CategoryId = null) : IRequest<List<ProductDto>>;
