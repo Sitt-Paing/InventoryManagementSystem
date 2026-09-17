cc91c55600e9723ddf365a080b486a6fb0024f9d
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:13:18 +0630

    Delete DeleteSupplierCommand.cs

--- src/InventoryManagementSystem.Application/Suppliers/Commands/DeleteSupplier/DeleteSupplierCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using InventoryManagementSystem.Application.Suppliers.DTOs;
-using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace InventoryManagementSystem.Application.Suppliers.Commands.DeleteSupplier;
-
-public record DeleteSupplierCommand(int id) : IRequest<SupplierDto>;
