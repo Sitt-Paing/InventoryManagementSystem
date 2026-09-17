ff9a8b6f59311939aeb1672e6cd805318ff1ed8d
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:13:58 +0630

    Add GetSuppliersQuery.cs

---
+++ src/InventoryManagementSystem.Application/Master/Suppliers/Queries/GetSuppliers/GetSuppliersQuery.cs
@@ -0,0 +1,9 @@
+﻿using InventoryManagementSystem.Application.Master.Suppliers.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagementSystem.Application.Master.Suppliers.Queries.GetSuppliers;
+
+public record GetSuppliersQuery() : IRequest<List<SupplierDto>>;
