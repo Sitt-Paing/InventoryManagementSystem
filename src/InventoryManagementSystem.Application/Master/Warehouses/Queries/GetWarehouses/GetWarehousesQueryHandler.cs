b4149cd55ed337191a9933108e5cbdb96c2140a1
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:14:17 +0630

    Add GetWarehousesQueryHandler.cs

---
+++ src/InventoryManagementSystem.Application/Master/Warehouses/Queries/GetWarehouses/GetWarehousesQueryHandler.cs
@@ -0,0 +1,46 @@
+using InventoryManagementSystem.Application.Common.Interfaces;
+using InventoryManagementSystem.Application.Master.Warehouses.DTOs;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Application.Master.Warehouses.Queries.GetWarehouses;
+
+public class GetWarehousesQueryHandler : IRequestHandler<GetWarehousesQuery, List<WarehouseDto>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetWarehousesQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<WarehouseDto>> Handle(GetWarehousesQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.Warehouses
+            .AsNoTracking()
+            .Where(x => !x.DeletedOn.HasValue)
+            .Select(w => new WarehouseDto
+            {
+                Id = w.Id,
+                WarehouseCode = w.WarehouseCode,
+                Name = w.Name,
+                Address = w.Address,
+                ContactPerson = w.ContactPerson,
+                Phone = w.Phone,
+                Email = w.Email,
+                Capacity = w.Capacity,
+                Status = w.Status,
+                CreatedOn = w.CreatedOn,
+                CreatedBy = w.CreatedBy,
+                UpdatedOn = w.UpdatedOn,
+                UpdatedBy = w.UpdatedBy,
+                DeletedOn = w.DeletedOn,
+                DeletedBy = w.DeletedBy
+            })
+            .ToListAsync(cancellationToken);
+    }
+}
