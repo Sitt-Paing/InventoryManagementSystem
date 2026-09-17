81e73006e67335f9268f3df58d2cc284447196c5
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:13:35 +0630

    Delete GetWarehouseLocationByIdQuery.cs

--- src/InventoryManagementSystem.Application/WarehouseLocations/Queries/GetWarehouseLocationById/GetWarehouseLocationByIdQuery.cs
+++
@@ -1,6 +0,0 @@
-using InventoryManagementSystem.Application.WarehouseLocations.DTOs;
-using MediatR;
-
-namespace InventoryManagementSystem.Application.WarehouseLocations.Queries.GetWarehouseLocationById;
-
-public record class GetWarehouseLocationByIdQuery(int Id) : IRequest<WarehouseLocationDto?>;
