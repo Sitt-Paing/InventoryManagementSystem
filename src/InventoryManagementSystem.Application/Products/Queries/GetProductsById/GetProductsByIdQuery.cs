e80340a9aa03af54d26763f1df9689474470ac7d
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:13:16 +0630

    Delete GetProductsByIdQuery.cs

--- src/InventoryManagementSystem.Application/Products/Queries/GetProductsById/GetProductsByIdQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using InventoryManagementSystem.Application.Products.DTOs;
-using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace InventoryManagementSystem.Application.Products.Queries.GetProductsById;
-
-public record GetProductByIdQuery(Guid ProductId) : IRequest<ProductDto>;
