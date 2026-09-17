2dc2646f6f243d92e53f79cb0444a7fe2f4b71fb
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:13:08 +0630

    Delete DeleteProductUomConversionCommand.cs

--- src/InventoryManagementSystem.Application/ProductUomConversions/Commands/DeleteProductUomConversion/DeleteProductUomConversionCommand.cs
+++
@@ -1,6 +0,0 @@
-using InventoryManagementSystem.Application.ProductUomConversions.DTOs;
-using MediatR;
-
-namespace InventoryManagementSystem.Application.ProductUomConversions.Commands.DeleteProductUomConversion;
-
-public record DeleteProductUomConversionCommand(long Id) : IRequest<ProductUomConversionDto?>;
