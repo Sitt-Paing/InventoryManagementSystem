743de2fdb58d9a9620b218ded91b2c4e3508386c
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:13:25 +0630

    Delete GetUnitOfMeasureByIdQuery.cs

--- src/InventoryManagementSystem.Application/UnitOfMeasures/Queries/GetUnitOfMeasureById/GetUnitOfMeasureByIdQuery.cs
+++
@@ -1,6 +0,0 @@
-using InventoryManagementSystem.Application.UnitOfMeasures.DTOs;
-using MediatR;
-
-namespace InventoryManagementSystem.Application.UnitOfMeasures.Queries.GetUnitOfMeasureById;
-
-public record GetUnitOfMeasureByIdQuery(long Id) : IRequest<UnitOfMeasureDto?>;
