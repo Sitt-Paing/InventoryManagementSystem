c7041047df5e45abd567433eadbeaa8b2711567b
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:13:52 +0630

    Update Product.cs

--- src/InventoryManagementSystem.Domain/Entities/Product.cs
+++ src/InventoryManagementSystem.Domain/Entities/Product.cs
@@ -23,13 +23,19 @@ public partial class Product : BaseAuditableEntity<Guid>
 
     public decimal SellingPrice { get; set; }
 
-    public int CurrentStock { get; set; }
+    public decimal CurrentStock { get; set; }
 
-    public int ReorderLevel { get; set; }
+    public decimal ReorderLevel { get; set; }
 
-    public int ReorderQuantity { get; set; }
+    public decimal ReorderQuantity { get; set; }
 
     public decimal Tax { get; set; }
+
+    public long BaseUomId { get; set; }
+
+    public long? PurchaseUomId { get; set; }
+
+    public long? SaleUomId { get; set; }
 
     public bool Status { get; set; } = true;
 
@@ -37,6 +43,15 @@ public partial class Product : BaseAuditableEntity<Guid>
 
     [JsonIgnore]
     public virtual Category Category { get; set; } = null!;
+
+    [JsonIgnore]
+    public virtual UnitOfMeasure BaseUom { get; set; } = null!;
+
+    [JsonIgnore]
+    public virtual UnitOfMeasure? PurchaseUom { get; set; }
+
+    [JsonIgnore]
+    public virtual UnitOfMeasure? SaleUom { get; set; }
 
     [JsonIgnore]
     public virtual ICollection<StockTransaction> StockTransactions { get; set; } = new List<StockTransaction>();
