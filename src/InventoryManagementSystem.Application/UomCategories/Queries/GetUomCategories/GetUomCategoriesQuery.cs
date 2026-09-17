4962d9c094de09f490b1b4b62ca9fab22bdef108
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:13:30 +0630

    Delete GetUomCategoriesQuery.cs

--- src/InventoryManagementSystem.Application/UomCategories/Queries/GetUomCategories/GetUomCategoriesQuery.cs
+++
@@ -1,7 +0,0 @@
-using InventoryManagementSystem.Application.UomCategories.DTOs;
-using MediatR;
-using System.Collections.Generic;
-
-namespace InventoryManagementSystem.Application.UomCategories.Queries.GetUomCategories;
-
-public record GetUomCategoriesQuery : IRequest<List<UomCategoryDto>>;
