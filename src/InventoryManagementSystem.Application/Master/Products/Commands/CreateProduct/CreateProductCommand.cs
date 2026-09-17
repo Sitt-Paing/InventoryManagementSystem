93e1334d15eff8a1a54939c18ae3e3450bc9f606
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:09:11 +0630

    Create CreateProductCommand.cs

---
+++ src/InventoryManagementSystem.Application/Master/Products/Commands/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,26 @@
+using InventoryManagementSystem.Application.Master.Products.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagementSystem.Application.Master.Products.Commands.CreateProduct;
+
+public record CreateProductCommand(
+    string Name,
+    long CategoryId,
+    long BaseUomId,
+    decimal SellingPrice = 0,
+    string? Sku = null,
+    decimal CurrentStock = 0,
+    decimal ReorderLevel = 0,
+    string? Barcode = null,
+    string? Brand = null,
+    string? Unit = null,
+    long? PurchaseUomId = null,
+    long? SaleUomId = null,
+    decimal CostPrice = 0,
+    decimal ReorderQuantity = 0,
+    decimal Tax = 0,
+    bool Status = true,
+    string? Description = null) : IRequest<ProductDto>;
