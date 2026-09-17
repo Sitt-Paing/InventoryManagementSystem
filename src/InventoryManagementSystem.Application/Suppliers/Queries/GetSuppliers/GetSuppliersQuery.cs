86c84e53ff49d25b846fc8e013bd1abb9b27c66f
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:13:21 +0630

    Delete GetSuppliersQuery.cs

--- src/InventoryManagementSystem.Application/Suppliers/Queries/GetSuppliers/GetSuppliersQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using InventoryManagementSystem.Application.Suppliers.DTOs;
-using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace InventoryManagementSystem.Application.Suppliers.Queries.GetSuppliers;
-
-public record GetSuppliersQuery() : IRequest<List<SupplierDto>>;
