edd64eacdf27a3054efbf15c43298305a5fb3986
Sitt Paing <sittp9288@gmail.com>
2026-09-17 11:08:51 +0630

    Create CategoryDto.cs

---
+++ src/InventoryManagementSystem.Application/Master/Categories/DTOs/CategoryDto.cs
@@ -0,0 +1,15 @@
+namespace InventoryManagementSystem.Application.Master.Categories.DTOs;
+
+public record CategoryDto
+{
+    public long Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime? CreatedOn { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? UpdatedOn { get; set; }
+    public string? UpdatedBy { get; set; }
+    public DateTime? DeletedOn { get; set; }
+    public string? DeletedBy { get; set; }
+}
