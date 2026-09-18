db2aab584dcafad38dcc199e09f9b1c77d855f73
Sitt Paing <sittp9288@gmail.com>
2026-09-18 16:08:07 +0630

    Update InventoryManagementDbContext.cs

--- src/InventoryManagementSystem.Infrastructure/Persistence/InventoryManagementDbContext.cs
+++ src/InventoryManagementSystem.Infrastructure/Persistence/InventoryManagementDbContext.cs
@@ -42,6 +42,8 @@ public partial class InventoryManagementDbContext : DbContext
 
     public virtual DbSet<WarehouseLocation> WarehouseLocations { get; set; }
 
+    public virtual DbSet<WarehouseStocks> WarehouseStocks { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<AspNetRole>(entity =>
