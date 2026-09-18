4f2b78f016a87e8fb14328a073aa2cf99fffef07
Sitt Paing <sittp9288@gmail.com>
2026-09-18 09:45:02 +0630

    Create UpdateStockTransactionsCommand.cs

---
+++ src/InventoryManagementSystem.Application/Process/StockTransactions/Command/UpdateStockTransactions/UpdateStockTransactionsCommand.cs
@@ -0,0 +1,18 @@
+using System;
+using InventoryManagementSystem.Application.Process.StockTransactions.DTOs;
+using MediatR;
+
+namespace InventoryManagementSystem.Application.Process.StockTransactions.Command.UpdateStockTransactions;
+
+public record class UpdateStockTransactionsCommand(
+    long Id,
+    Guid ProductId,
+    string UserId,
+    int WarehouseId,
+    int WarehouseLocationId,
+    decimal Quantity,
+    string TransactionType,
+    DateTime TransactionDate,
+    long? ReferenceNo,
+    string? Note
+) : IRequest<StockTrasactionsDto>;
