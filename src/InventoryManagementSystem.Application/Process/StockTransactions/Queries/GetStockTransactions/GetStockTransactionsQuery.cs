b06376a579dfcc5ae002c7cd6de300e78a1e2da0
Sitt Paing <sittp9288@gmail.com>
2026-09-18 09:45:22 +0630

    Create GetStockTransactionsQuery.cs

---
+++ src/InventoryManagementSystem.Application/Process/StockTransactions/Queries/GetStockTransactions/GetStockTransactionsQuery.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using InventoryManagementSystem.Application.Process.StockTransactions.DTOs;
+using MediatR;
+
+namespace InventoryManagementSystem.Application.Process.StockTransactions.Queries.GetStockTransactions;
+
+public record class GetStockTransactionsQuery(
+    string? TransactionType = null,
+    DateTime? Date = null,
+    Guid? ProductId = null,
+    int? WarehouseId = null
+) : IRequest<List<StockTrasactionsDto>>;
