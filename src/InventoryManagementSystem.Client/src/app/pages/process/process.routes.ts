c60adeae72735a17613fe55f46abfa7ba744b6e3
Sitt Paing <sittp9288@gmail.com>
2026-09-18 09:45:46 +0630

    Update process.routes.ts

--- src/InventoryManagementSystem.Client/src/app/pages/process/process.routes.ts
+++ src/InventoryManagementSystem.Client/src/app/pages/process/process.routes.ts
@@ -3,6 +3,6 @@ import { Routes } from '@angular/router';
 export default [
   {
     path: 'stock-transactions',
-    loadComponent: () => import('../stock-transactions/stock-transactions').then(m => m.StockTransactionsComponent)
+    loadComponent: () => import('./stock-transactions/stock-transactions').then(m => m.StockTransactionsComponent)
   }
 ] as Routes;
