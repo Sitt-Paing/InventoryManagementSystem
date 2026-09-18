0d16e63aef5d9ef7ded31b696fec44c9c0f353f7
Sitt Paing <sittp9288@gmail.com>
2026-09-18 09:45:16 +0630

    Create GetStockTransactionByIdQuery.cs

---
+++ src/InventoryManagementSystem.Application/Process/StockTransactions/Queries/GetStockTransactionById/GetStockTransactionByIdQuery.cs
@@ -0,0 +1,6 @@
+using InventoryManagementSystem.Application.Process.StockTransactions.DTOs;
+using MediatR;
+
+namespace InventoryManagementSystem.Application.Process.StockTransactions.Queries.GetStockTransactionById;
+
+public record class GetStockTransactionByIdQuery(long Id) : IRequest<StockTrasactionsDto?>;
