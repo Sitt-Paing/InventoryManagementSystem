ae921539fbc165549a9a8f6cc971b952ca2a2713
Sitt Paing <sittp9288@gmail.com>
2026-09-18 16:08:02 +0630

    Create WarehouseStock.cs

---
+++ src/InventoryManagementSystem.Domain/Entities/WarehouseStock.cs
@@ -0,0 +1,19 @@
+﻿using InventoryManagementSystem.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace InventoryManagementSystem.Domain.Entities;
+
+public partial class WarehouseStocks : BaseAuditableEntity<long>
+{
+    public Guid ProductId { get; set; }
+    public int WarehouseId { get; set; }
+    public decimal Quantity { get; set; }
+
+    [JsonIgnore]
+    public virtual Product Product { get; set; } = null!;
+    [JsonIgnore]
+    public virtual Warehouse Warehouse { get; set; } = null!;
+}
