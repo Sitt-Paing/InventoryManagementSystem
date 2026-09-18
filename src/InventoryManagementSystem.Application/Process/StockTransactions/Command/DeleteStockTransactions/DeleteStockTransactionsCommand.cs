26b8aed0f63d742bd8d13b72e083cb1ba7bd7695
Sitt Paing <sittp9288@gmail.com>
2026-09-18 10:08:32 +0630

    Update DeleteStockTransactionsCommand.cs

--- src/InventoryManagementSystem.Application/Process/StockTransactions/Command/DeleteStockTransactions/DeleteStockTransactionsCommand.cs
+++ src/InventoryManagementSystem.Application/Process/StockTransactions/Command/DeleteStockTransactions/DeleteStockTransactionsCommand.cs
@@ -1,5 +1,6 @@
+using InventoryManagementSystem.Application.Process.StockTransactions.DTOs;
 using MediatR;
 
 namespace InventoryManagementSystem.Application.Process.StockTransactions.Command.DeleteStockTransactions;
 
-public record class DeleteStockTransactionsCommand(long Id, string? UserId) : IRequest<bool>;
+public record class DeleteStockTransactionsCommand(long Id, string? UserId = null) : IRequest<StockTransactionsDto?>;
