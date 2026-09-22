6c646cc139ecd42d8eca0a4c371a8cd32a70d560
Sitt Paing <sittp9288@gmail.com>
2026-09-22 11:44:01 +0630

    Update warehouse.service.ts

--- src/InventoryManagementSystem.Client/src/app/core/services/master/warehouse.service.ts
+++ src/InventoryManagementSystem.Client/src/app/core/services/master/warehouse.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 import { RootModel } from '../../models/root.model';
-import { WarehouseModel } from '../../models/warehouse.model';
+import { WarehouseModel } from '../../models/master/warehouse.model';
 
 @Injectable({
   providedIn: 'root',
