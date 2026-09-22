b0d597dfd2f37e9f9edd776d3edc065367776d50
Sitt Paing <sittp9288@gmail.com>
2026-09-22 11:43:35 +0630

    Update category.service.ts

--- src/InventoryManagementSystem.Client/src/app/core/services/master/category.service.ts
+++ src/InventoryManagementSystem.Client/src/app/core/services/master/category.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { environment } from "../../../../environments/environment";
-import { CategoryModel } from "../../models/category.model";
+import { CategoryModel } from "../../models/master/category.model";
 import { RootModel } from "../../models/root.model";
 
 @Injectable({
