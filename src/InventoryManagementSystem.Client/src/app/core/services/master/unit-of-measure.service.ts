c980feff3fa4632ef6a4730a797162c9e0835f2a
Sitt Paing <sittp9288@gmail.com>
2026-09-22 11:43:47 +0630

    Update unit-of-measure.service.ts

--- src/InventoryManagementSystem.Client/src/app/core/services/master/unit-of-measure.service.ts
+++ src/InventoryManagementSystem.Client/src/app/core/services/master/unit-of-measure.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 import { RootModel } from '../../models/root.model';
-import { UnitOfMeasureModel } from '../../models/unit-of-measure.model';
+import { UnitOfMeasureModel } from '../../models/master/unit-of-measure.model';
 
 @Injectable({
   providedIn: 'root'
