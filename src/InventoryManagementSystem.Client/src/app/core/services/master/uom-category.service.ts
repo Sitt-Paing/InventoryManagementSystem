ea22dd665b34ab0152f967170646b44a65d6800c
Sitt Paing <sittp9288@gmail.com>
2026-09-22 11:43:55 +0630

    Update uom-category.service.ts

--- src/InventoryManagementSystem.Client/src/app/core/services/master/uom-category.service.ts
+++ src/InventoryManagementSystem.Client/src/app/core/services/master/uom-category.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 import { RootModel } from '../../models/root.model';
-import { UomCategoryModel } from '../../models/uom-category.model';
+import { UomCategoryModel } from '../../models/master/uom-category.model';
 
 @Injectable({
   providedIn: 'root'
