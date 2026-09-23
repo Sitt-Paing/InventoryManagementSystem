6f1b6edb9b23037a3b61787f8c133a7a012ce1e9
Sitt Paing <sittp9288@gmail.com>
2026-09-23 16:16:10 +0630

    Create purchase-order.model.ts

---
+++ src/InventoryManagementSystem.Client/src/app/core/models/process/purchase-order.model.ts
@@ -0,0 +1,50 @@
+export interface PurchaseOrderItemModel {
+  id?: number;
+  purchaseOrderId?: string;
+  productId: string;
+  productName?: string;
+  productSku?: string;
+  quantity: number;
+  unitPrice: number;
+  uomId: number;
+  uomName?: string;
+  receivedQuantity?: number;
+  subTotal?: number;
+  createdOn?: Date | null;
+  createdBy?: string | null;
+  updatedOn?: Date | null;
+  updatedBy?: string | null;
+}
+
+export interface PurchaseOrderModel {
+  id?: string;
+  purchaseOrderNo: string;
+  supplierId: number;
+  supplierName?: string;
+  supplierCode?: string;
+  warehouseId: number;
+  warehouseName?: string;
+  orderDate: Date | string;
+  expectedDate: Date | string;
+  status: boolean;
+  totalAmount?: number;
+  totalItems?: number;
+  items: PurchaseOrderItemModel[];
+  createdOn?: Date | null;
+  createdBy?: string | null;
+  updatedOn?: Date | null;
+  updatedBy?: string | null;
+}
+
+export interface PurchaseOrderFilterModel {
+  q?: string | null;
+  startDate?: string | null;
+  endDate?: string | null;
+  supplierId?: number | null;
+  warehouseId?: number | null;
+  status?: boolean | null;
+  sortField?: string | null;
+  order?: number | null;
+  pageNumber?: number;
+  pageSize?: number;
+}
