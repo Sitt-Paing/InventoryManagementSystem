f8a59d76a876244ff2a4a0d06cba8bfa47331f5e
Sitt Paing <sittp9288@gmail.com>
2026-09-23 16:15:25 +0630

    Create UpdatePurchaseOrderCommandValidator.cs

---
+++ src/InventoryManagementSystem.Application/Process/PurchaseOrders/Commands/UpdatePurchaseOrder/UpdatePurchaseOrderCommandValidator.cs
@@ -0,0 +1,47 @@
+using FluentValidation;
+
+namespace InventoryManagementSystem.Application.Process.PurchaseOrders.Commands.UpdatePurchaseOrder;
+
+public class UpdatePurchaseOrderCommandValidator : AbstractValidator<UpdatePurchaseOrderCommand>
+{
+    public UpdatePurchaseOrderCommandValidator()
+    {
+        RuleFor(v => v.Id)
+            .NotEmpty().WithMessage("Purchase Order ID is required.");
+
+        RuleFor(v => v.PurchaseOrderNo)
+            .NotEmpty().WithMessage("Purchase Order Number is required.")
+            .MaximumLength(50).WithMessage("Purchase Order Number cannot exceed 50 characters.");
+
+        RuleFor(v => v.SupplierId)
+            .GreaterThan(0).WithMessage("A valid Supplier must be selected.");
+
+        RuleFor(v => v.WarehouseId)
+            .GreaterThan(0).WithMessage("A valid Warehouse must be selected.");
+
+        RuleFor(v => v.OrderDate)
+            .NotEmpty().WithMessage("Order Date is required.");
+
+        RuleFor(v => v.ExpectedDate)
+            .NotEmpty().WithMessage("Expected Date is required.")
+            .GreaterThanOrEqualTo(v => v.OrderDate).WithMessage("Expected Date must be on or after Order Date.");
+
+        RuleFor(v => v.Items)
+            .NotEmpty().WithMessage("At least one purchase order item is required.");
+
+        RuleForEach(v => v.Items).ChildRules(item =>
+        {
+            item.RuleFor(i => i.ProductId)
+                .NotEmpty().WithMessage("Product is required for each item.");
+
+            item.RuleFor(i => i.Quantity)
+                .GreaterThan(0).WithMessage("Quantity must be greater than 0.");
+
+            item.RuleFor(i => i.UnitPrice)
+                .GreaterThanOrEqualTo(0).WithMessage("Unit Price must be 0 or greater.");
+
+            item.RuleFor(i => i.UomId)
+                .GreaterThan(0).WithMessage("UOM is required for each item.");
+        });
+    }
+}
