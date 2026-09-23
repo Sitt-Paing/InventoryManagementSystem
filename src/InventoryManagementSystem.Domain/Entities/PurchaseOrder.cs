587053a87155b419a1fa51519581af03fece035c
Sitt Paing <sittp9288@gmail.com>
2026-09-23 16:16:44 +0630

    Create PurchaseOrder.cs

---
+++ src/InventoryManagementSystem.Domain/Entities/PurchaseOrder.cs
@@ -0,0 +1,24 @@
+﻿using InventoryManagementSystem.Domain.Common;
+using System;
+using System.Text.Json.Serialization;
+
+namespace InventoryManagementSystem.Domain.Entities;
+
+public class PurchaseOrder : BaseAuditableEntity<Guid>
+{
+    public string PurchaseOrderNo { get; set; } = string.Empty;
+    public int SupplierId { get; set; }
+    public int WarehouseId { get; set; }
+    public DateTime OrderDate { get; set; }
+    public DateTime ExpectedDate { get; set; }
+    public bool Status { get; set; }
+    public decimal TotalAmount { get; set; }
+
+    [JsonIgnore]
+    public virtual Supplier? Supplier { get; set; }
+    [JsonIgnore]
+    public virtual Warehouse? Warehouse { get; set; }
+
+    [JsonIgnore]
+    public virtual ICollection<PurchaseOrderItem> Items { get; set; } = new List<PurchaseOrderItem>();
+}
