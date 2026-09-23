448b0fef549a4f920f8e355b1cd766f49fade840
Sitt Paing <sittp9288@gmail.com>
2026-09-23 16:16:18 +0630

    Update process.routes.ts

--- src/InventoryManagementSystem.Client/src/app/pages/process/process.routes.ts
+++ src/InventoryManagementSystem.Client/src/app/pages/process/process.routes.ts
@@ -4,5 +4,9 @@ export default [
   {
     path: 'stock-transactions',
     loadComponent : () => import('./stock-transactions/stock-transactions').then(m => m.StockTransactionsComponent),
+  },
+  {
+    path: 'purchase-orders',
+    loadComponent: () => import('./purchase-orders/purchase-orders').then(m => m.PurchaseOrdersComponent),
   }
 ] as Routes;
