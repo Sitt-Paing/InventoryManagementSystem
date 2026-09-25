3c678b48263b556992c5b2448980025c98e3095c
Sitt Paing <sittp9288@gmail.com>
2026-09-25 13:32:26 +0630

    Update mm.ts

--- src/InventoryManagementSystem.Client/src/app/core/i18n/mm.ts
+++ src/InventoryManagementSystem.Client/src/app/core/i18n/mm.ts
@@ -17,6 +17,7 @@ export const MM_DICTIONARY: Record<string, any> = {
     PROCESS: 'လုပ်ငန်းစဉ်',
     STOCK_TRANSACTIONS: 'စတော့ လှုပ်ရှားမှုများ',
     PURCHASE_ORDERS: 'အဝယ်များ',
+    GOOD_RECEIPTS: 'ကုန်လက်ခံလွှာ (GRN)',
     SETTINGS: 'ဆက်တင်များ',
     USER_MANAGEMENT: 'အသုံးပြုသူ စီမံခန့်ခွဲမှု'
   },
