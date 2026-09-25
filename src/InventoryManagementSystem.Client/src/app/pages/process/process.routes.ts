b927068587ab74cb71e200f85354f8d7374c97bb
Sitt Paing <sittp9288@gmail.com>
2026-09-25 13:32:53 +0630

    Update process.routes.ts

--- src/InventoryManagementSystem.Client/src/app/pages/process/process.routes.ts
+++ src/InventoryManagementSystem.Client/src/app/pages/process/process.routes.ts
@@ -8,5 +8,9 @@ export default [
   {
     path: 'purchase-orders',
     loadComponent: () => import('./purchase-orders/purchase-orders').then(m => m.PurchaseOrdersComponent),
+  },
+  {
+    path: 'good-receipts',
+    loadComponent: () => import('./good-receipts/good-receipts').then(m => m.GoodReceiptsComponent),
   }
 ] as Routes;
