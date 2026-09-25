d9d244600aa0fd58230e2e1992d09dc38a974c69
Sitt Paing <sittp9288@gmail.com>
2026-09-25 14:14:57 +0630

    Update GetPurchaseOrdersQuery.cs

--- src/InventoryManagementSystem.Application/Process/PurchaseOrders/Queries/GetPurchaseOrders/GetPurchaseOrdersQuery.cs
+++ src/InventoryManagementSystem.Application/Process/PurchaseOrders/Queries/GetPurchaseOrders/GetPurchaseOrdersQuery.cs
@@ -1,6 +1,7 @@
 using System;
 using InventoryManagementSystem.Application.Common.Models;
 using InventoryManagementSystem.Application.Process.PurchaseOrders.DTOs;
+using InventoryManagementSystem.Domain.Enums;
 using MediatR;
 
 namespace InventoryManagementSystem.Application.Process.PurchaseOrders.Queries.GetPurchaseOrders;
@@ -10,7 +11,7 @@ public record GetPurchaseOrdersQuery(
     DateTime? OrderDate = null,
     int? SupplierId = null,
     int? WarehouseId = null,
-    bool? Status = null,
+    PurchaseOrderStatus? Status = null,
     string? SortField = null,
     int Order = -1,
     int PageNumber = 1,
