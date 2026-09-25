ee687ae7c50aeaf40049ae7fd05d98d4f206e4a2
Sitt Paing <sittp9288@gmail.com>
2026-09-25 11:37:46 +0630

    Create GoodReceipt.cs

---
+++ src/InventoryManagementSystem.Domain/Entities/GoodReceipt.cs
@@ -0,0 +1,28 @@
+﻿using InventoryManagementSystem.Domain.Common;
+using System.Text.Json.Serialization;
+
+namespace InventoryManagementSystem.Domain.Entities;
+
+public partial class GoodReceipt: BaseAuditableEntity<Guid>
+{
+    public string ReceiptNo { get; set; } = string.Empty;
+    public int WarehouseId { get; set; }
+    public Guid PurchaseOrderId { get; set; }
+    public int SupplierId { get; set; }
+    public DateTime ReceiptDate { get; set; }
+    public bool Status { get; set; }
+    public string ReceivedBy { get; set; } = string.Empty;
+    public string? Note { get; set; }
+
+    [JsonIgnore]
+    public virtual Warehouse? Warehouse { get; set; }
+
+    [JsonIgnore]
+    public virtual PurchaseOrder? PurchaseOrder { get; set; }
+
+    [JsonIgnore]
+    public virtual Supplier? Supplier { get; set; }
+
+    [JsonIgnore]
+    public virtual ICollection<GoodReceiptItem> Items { get; set; } = new List<GoodReceiptItem>();
+}
