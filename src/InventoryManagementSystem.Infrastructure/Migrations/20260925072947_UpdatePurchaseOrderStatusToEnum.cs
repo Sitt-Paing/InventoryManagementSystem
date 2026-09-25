f699754a991ee7299b743677d58d673b872ab89d
Sitt Paing <sittp9288@gmail.com>
2026-09-25 14:15:28 +0630

    Create 20260925072947_UpdatePurchaseOrderStatusToEnum.cs

---
+++ src/InventoryManagementSystem.Infrastructure/Migrations/20260925072947_UpdatePurchaseOrderStatusToEnum.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryManagementSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePurchaseOrderStatusToEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "PurchaseOrders",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Status",
+                table: "PurchaseOrders",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
