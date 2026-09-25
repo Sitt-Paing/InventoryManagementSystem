2f03904294eab9d7b276b24ef16ab7639343b0f5
Sitt Paing <sittp9288@gmail.com>
2026-09-25 14:14:52 +0630

    Update CreatePurchaseOrderCommand.cs

--- src/InventoryManagementSystem.Application/Process/PurchaseOrders/Commands/CreatePurchaseOrder/CreatePurchaseOrderCommand.cs
+++ src/InventoryManagementSystem.Application/Process/PurchaseOrders/Commands/CreatePurchaseOrder/CreatePurchaseOrderCommand.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using InventoryManagementSystem.Application.Process.PurchaseOrders.DTOs;
+using InventoryManagementSystem.Domain.Enums;
 using MediatR;
 
 namespace InventoryManagementSystem.Application.Process.PurchaseOrders.Commands.CreatePurchaseOrder;
@@ -19,6 +20,6 @@ public record CreatePurchaseOrderCommand(
     int WarehouseId,
     DateTime OrderDate,
     DateTime ExpectedDate,
-    bool Status,
+    PurchaseOrderStatus Status,
     List<CreatePurchaseOrderItemInput> Items
 ) : IRequest<PurchaseOrderDto>;
