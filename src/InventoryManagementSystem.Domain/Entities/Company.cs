1504549cc890715657ddcede168b23329776ba60
Sitt Paing <sittp9288@gmail.com>
2026-09-26 15:57:16 +0630

    Create Company.cs

---
+++ src/InventoryManagementSystem.Domain/Entities/Company.cs
@@ -0,0 +1,20 @@
+﻿using InventoryManagementSystem.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace InventoryManagementSystem.Domain.Entities;
+
+public partial class Company : BaseAuditableEntity<int>
+{
+    public string CompanyName { get; set; } = string.Empty!;
+    public string Email { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string ContactPerson { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public bool IsActive { get; set; }
+
+    [JsonIgnore]
+    public virtual ICollection<AspNetUser> AspNetUsers { get; set; } = new List<AspNetUser>();
+}
